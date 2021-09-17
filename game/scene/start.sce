01a578807c8db8d455fee035e3e73bff617e64a3
MakinoharaShoko <salufthansa@foxmail.com>
2021-09-17 09:36:50 +0800

    0.13
    支持背景与立绘变换

--- game/scene/start.sce
+++ game/scene/start.sce
@@ -1,2 +1,6 @@
-A:真是安静啊
-A:街上什么人也没有
+A:真是安静啊;
+A:街上什么人也没有;
+changeBG:testBG01.jpg;
+A:山不会向我走来，但是我可以走向山;
+changeP:123;
+changeBG:testBG02.jpg;
--- res/script/WG_main.js
+++ res/script/WG_main.js
@@ -16,6 +16,7 @@ function getScene() {
                 currentScene = getScReq.responseText;
                 currentScene = currentScene.split('\n');
                 for (let i = 0;i<currentScene.length;i++){
+                    currentScene[i] = currentScene[i].split(";")[0];
                     currentScene[i] = currentScene[i].split(":");
                 }
                 console.log('Read scene complete.');
@@ -34,8 +35,25 @@ function processSentence(i){
         return currentScene[i][0]+": "+currentScene[i][1];
 }
 
-function nextSentenceProcessor(){
-    let changedText = <p>{processSentence(currentSentence)}</p>
-    ReactDOM.render(changedText,document.getElementById('SceneText'));
+function nextSentenceProcessor() {
+    if(currentSentence >= currentScene.length){
+        return;
+    }
+    let thisSentence = currentScene[currentSentence];
+    let command = thisSentence[0];
+    console.log(command)
+    if (command === 'changeBG') {
+        console.log('Now change background to ' + "url('/game/background/" + thisSentence[1] + "')");
+        document.getElementById('mainBackground').style.backgroundImage = "url('/game/background/" + thisSentence[1] + "')";
+    }
+    else if(command === 'changeP'){
+        let changedP = <img src="/game/figure/testFigure01.png" alt='figure' className='p_center'/>
+        console.log('now changing person');
+        ReactDOM.render(changedP,document.getElementById('figureImage'));
+    }
+    else {
+        let changedText = <p>{processSentence(currentSentence)}</p>
+        ReactDOM.render(changedText, document.getElementById('SceneText'));
+    }
     currentSentence = currentSentence+1;
 }
