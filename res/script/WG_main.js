01245c927e766797199e21a0663f6298fc06c0a6
MakinoharaShoko <salufthansa@foxmail.com>
2021-09-21 11:01:47 +0800

    0.31
    添加存档系统

--- res/script/WG_main.js
+++ res/script/WG_main.js
@@ -2,6 +2,108 @@ var currentScene ='';
 var currentSceneIndex = 0;
 var currentSentence = 0;
 var currentText = 0;
+var currentInfo ={
+    SceneName:'',
+    SentenceID:0,
+    bg_Name:'',
+    fig_Name:'',
+    showText:'',
+    showName:'',
+    command:'',
+    choose:'',
+    currentText:0
+}
+
+
+var Saves=[];
+
+function LoadSavedGame(index) {
+    let save = Saves[index];
+    //get Scene:
+    let url = '/game/scene/'
+    url = url + save['SceneName'];
+    currentScene ='';
+    currentText = 0;
+
+    let getScReq = null;
+    getScReq = new XMLHttpRequest();
+
+    if (getScReq != null) {
+        getScReq.open("get",url , true);
+        getScReq.send();
+        getScReq.onreadystatechange = doResult; //设置回调函数
+    }
+    function doResult() {
+        if (getScReq.readyState === 4) { //4表示执行完成
+            if (getScReq.status === 200) { //200表示执行成功
+                currentScene = getScReq.responseText;
+                currentScene = currentScene.split('\n');
+                for (let i = 0;i<currentScene.length;i++){
+                    let tempSentence = currentScene[i].split(";")[0];
+                    let commandLength = tempSentence.split(":")[0].length;
+                    let command = currentScene[i].split(":")[0];
+                    let content = tempSentence.slice(commandLength+1);
+                    currentScene[i] = currentScene[i].split(":");
+                    currentScene[i][0] = command;
+                    currentScene[i][1] = content;
+                }
+                console.log('Read scene complete.');
+                // console.log(currentScene);
+                currentSentence = save["SentenceID"];
+                currentText = save["SentenceID"];
+                console.log("start:"+currentSentence)
+
+                //load saved scene:
+                let command = save["command"];
+                console.log('readSaves:'+command)
+                document.getElementById('mainBackground').style.backgroundImage = "url('game/background/" + save["bg_Name"] + "')";
+                if (save["fig_Name"] === 'none'){
+                    ReactDOM.render(<div/>,document.getElementById('figureImage'));
+                }else{
+                    let pUrl = "game/figure/"+save["fig_Name"];
+                    let changedP = <img src={pUrl} alt='figure' className='p_center'/>
+                    // console.log('now changing person');
+                    ReactDOM.render(changedP,document.getElementById('figureImage'));
+                }
+
+                if(command === 'choose'){
+                    document.getElementById('chooseBox').style.display = 'flex';
+                    let chooseItems =save["choose"];
+                    chooseItems = chooseItems.split("}")[0];
+                    chooseItems = chooseItems.split("{")[1];
+                    let selection = chooseItems.split(',')
+                    for (let i = 0;i<selection.length;i++){
+                        selection[i] = selection[i].split(":");
+                    }
+                    let elements = []
+                    for (let i = 0; i < selection.length; i++) {
+                        let temp = <div className='singleChoose' key={i} onClick={()=>{chooseScene(selection[i][1]);}}>{selection[i][0]}</div>
+                        elements.push(temp)
+                    }
+                    ReactDOM.render(<div>{elements}</div>,document.getElementById('chooseBox'))
+                    // return;
+                }
+                let changedName = <span>{save["showName"]}</span>
+                let textArray = save["showText"].split("");
+                // let changedText = <p>{processSentence(currentSentence)['text']}</p>
+                ReactDOM.render(changedName, document.getElementById('pName'));
+                currentText = save["currentText"];
+                showTextArray(textArray,currentText);
+                // currentText = currentText + 1;
+
+                // currentSentence = currentSentence+1;
+            }
+        }
+    }
+
+
+}
+
+function saveGame(index){
+    let tempInfo = JSON.stringify(currentInfo);
+    Saves[index] = JSON.parse(tempInfo);
+}
+
 function getScene(url) {
     currentScene ='';
     currentText = 0;
@@ -41,6 +143,7 @@ function getScene(url) {
 
 window.onload = function (){
     getScene("game/scene/start.txt");
+    currentInfo["SceneName"] = 'start.txt';
 }
 
 function processSentence(i){
@@ -49,6 +152,7 @@ function processSentence(i){
 }
 
 function nextSentenceProcessor() {
+
     if(currentSentence >= currentScene.length){
         return;
     }
@@ -58,26 +162,31 @@ function nextSentenceProcessor() {
     if (command === 'changeBG') {
         // console.log('Now change background to ' + "url('/game/background/" + thisSentence[1] + "')");
         document.getElementById('mainBackground').style.backgroundImage = "url('game/background/" + thisSentence[1] + "')";
+        currentInfo["bg_Name"]= thisSentence[1];
     }
     else if(command === 'changeP'){
         if (thisSentence[1] === 'none'){
             ReactDOM.render(<div/>,document.getElementById('figureImage'));
+            currentInfo["fig_Name"] = 'none';
         }else{
             let pUrl = "game/figure/"+thisSentence[1];
             let changedP = <img src={pUrl} alt='figure' className='p_center'/>
             // console.log('now changing person');
             ReactDOM.render(changedP,document.getElementById('figureImage'));
+            currentInfo["fig_Name"] = thisSentence[1];
         }
 
     }
     else if(command === 'changeP_next'){
         if (thisSentence[1] === 'none'){
             ReactDOM.render(<div/>,document.getElementById('figureImage'));
+            currentInfo["fig_Name"] = thisSentence[1];
         }else{
             let pUrl = "game/figure/"+thisSentence[1];
             let changedP = <img src={pUrl} alt='figure' className='p_center'/>
             // console.log('now changing person');
             ReactDOM.render(changedP,document.getElementById('figureImage'));
+            currentInfo["fig_Name"] = thisSentence[1];
         }
         currentSentence = currentSentence+1;
         nextSentenceProcessor();
@@ -86,17 +195,21 @@ function nextSentenceProcessor() {
     else if(command === 'changeBG_next'){
         document.getElementById('mainBackground').style.backgroundImage = "url('game/background/" + thisSentence[1] + "')";
         currentSentence = currentSentence+1;
+        currentInfo["bg_Name"] = thisSentence[1];
         nextSentenceProcessor();
         return;
     }
     else if(command === 'changeScene'){
         let sUrl = "game/scene/"+thisSentence[1];
+        currentInfo["SceneName"] = thisSentence[1];
         getScene(sUrl);
         return;
     }
     else if(command === 'choose'){
+        currentInfo["command"] = command;
         document.getElementById('chooseBox').style.display = 'flex';
         let chooseItems =thisSentence[1];
+        currentInfo["choose"]=chooseItems;
         chooseItems = chooseItems.split("}")[0];
         chooseItems = chooseItems.split("{")[1];
         let selection = chooseItems.split(',')
@@ -112,14 +225,19 @@ function nextSentenceProcessor() {
         return;
     }
     else {
+        currentInfo["command"] = processSentence(currentSentence)['name'];
+        currentInfo["showName"] = processSentence(currentSentence)['name'];
+        currentInfo["showText"] = processSentence(currentSentence)['text'];
         let changedName = <span>{processSentence(currentSentence)['name']}</span>
         let textArray = processSentence(currentSentence)['text'].split("");
         // let changedText = <p>{processSentence(currentSentence)['text']}</p>
         ReactDOM.render(changedName, document.getElementById('pName'));
         showTextArray(textArray,currentText+1);
         currentText = currentText + 1;
+        currentInfo["currentText"] = currentText;
     }
     currentSentence = currentSentence+1;
+    currentInfo["SentenceID"] = currentSentence;
 }
 
 function showTextArray(textArray,now){
@@ -128,7 +246,7 @@ function showTextArray(textArray,now){
     let i = 0;
     clearInterval(interval);
     var interval = setInterval(showSingle,35);
-    // console.log("now: "+now+" currentText: "+currentText)
+    console.log("now: "+now+" currentText: "+currentText)
     function showSingle() {
         let tempElement = <span key={i} className='singleWord'>{textArray[i]}</span>
         elementArray.push(tempElement);
@@ -171,6 +289,7 @@ function closeSettings(){
 
 function chooseScene(url){
     console.log(url);
+    currentInfo["SceneName"] = url;
     let sUrl = "game/scene/"+url;
     getScene(sUrl);
     document.getElementById("chooseBox").style.display="none"
