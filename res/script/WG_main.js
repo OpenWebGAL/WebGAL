0fc95708d2c460aadb21539c97cc6846292a073c
MakinoharaShoko <salufthansa@foxmail.com>
2021-10-09 21:22:12 +0800

    Update WG_main.js

--- res/script/WG_main.js
+++ res/script/WG_main.js
@@ -1186,12 +1186,12 @@ function closeBacklog(){
 }
 
 // 禁止F12
-document.onkeydown=function(e){
-        if(e.keyCode === 123){
-            e.returnValue=false
-            return false
-        }
-    }
+// document.onkeydown=function(e){
+//         if(e.keyCode === 123){
+//             e.returnValue=false
+//             return false
+//         }
+//     }
 //禁止右键菜单以及选择文字
 document.addEventListener('contextmenu', function(e) {
   e.preventDefault();
