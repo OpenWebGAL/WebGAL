cf09d8689689deb22ff662c815ca07698b3a9be3
MakinoharaShoko <salufthansa@foxmail.com>
2021-10-17 10:40:10 +0800

    3.04

--- res/script/jsx/WG_viewController.js
+++ res/script/jsx/WG_viewController.js
@@ -67,6 +67,99 @@ function VC_textShow(name,text){
     showTextArray(textArray);
 }
 
+function VC_restoreStatus(savedStatus) {
+    let command = savedStatus["command"];
+    if(savedStatus["bg_Name"]!=='')
+        document.getElementById('mainBackground').style.backgroundImage = "url('game/background/" + savedStatus["bg_Name"] + "')";
+    if (savedStatus["fig_Name"] === ''||savedStatus["fig_Name"] === 'none'){
+        ReactDOM.render(<div/>,document.getElementById('figureImage'));
+    }else{
+        let pUrl = "game/figure/"+savedStatus["fig_Name"];
+        let changedP = <img src={pUrl} alt='figure' className='p_center'/>
+        // console.log('now changing person');
+        ReactDOM.render(changedP,document.getElementById('figureImage'));
+    }
+    if (savedStatus["fig_Name_left"] === ''||savedStatus["fig_Name_left"] === 'none'){
+        ReactDOM.render(<div/>,document.getElementById('figureImage_left'));
+    }else{
+        let pUrl = "game/figure/"+savedStatus["fig_Name_left"];
+        let changedP = <img src={pUrl} alt='figure' className='p_center'/>
+        // console.log('now changing person');
+        ReactDOM.render(changedP,document.getElementById('figureImage_left'));
+    }
+    if (savedStatus["fig_Name_right"] === ''||savedStatus["fig_Name_right"] === 'none'){
+        ReactDOM.render(<div/>,document.getElementById('figureImage_right'));
+    }else{
+        let pUrl = "game/figure/"+savedStatus["fig_Name_right"];
+        let changedP = <img src={pUrl} alt='figure' className='p_center'/>
+        // console.log('now changing person');
+        ReactDOM.render(changedP,document.getElementById('figureImage_right'));
+    }
+    if(command === 'choose'||command === 'choose_label'){
+        let chooseItems =savedStatus["choose"];
+        chooseItems = chooseItems.split("}")[0];
+        chooseItems = chooseItems.split("{")[1];
+        let selection = chooseItems.split(',')
+        for (let i = 0;i<selection.length;i++){
+            selection[i] = selection[i].split(":");
+        }
+        let choose_mode = '';
+        switch (command){
+            case 'choose':
+                choose_mode = 'scene';
+                break;
+            case 'choose_label':
+                choose_mode = 'label';
+                break;
+        }
+        VC_choose(selection,choose_mode);
+    }
+    let changedName = <span>{savedStatus["showName"]}</span>
+    let textArray = savedStatus["showText"].split("");
+    ReactDOM.render(changedName, document.getElementById('pName'));
+    SyncCurrentStatus('vocal',savedStatus['vocal']);
+    if(getStatus('bgm') !== savedStatus['bgm']){
+        currentInfo['bgm'] = savedStatus['bgm'];
+        loadBGM();
+    }
+    playVocal();
+    showTextArray(textArray);
+}
+
+function VC_showSettings(){
+    let settingInterface = <div>
+        <div className="singleSettingItem">
+            <SettingButtons_font/>
+            <SettingButtons_speed/>
+            <div className={"deleteCookie"} onClick={()=>{showMesModel('你确定要清除缓存吗','要','不要',clearCookie)}}>清除所有设置选项以及存档</div>
+            <ImporterExporter />
+            <div>本作品由 WebGAL 强力驱动，<a href={"https://github.com/MakinoharaShoko/WebGAL"}>了解 WebGAL</a>。</div>
+            <br/>
+            <div className='settingItemTitle'>效果预览</div>
+        </div>
+    </div>
+    document.getElementById("settings").style.display = "flex";
+    document.getElementById("bottomBox").style.display = "none";
+    ReactDOM.render(settingInterface,document.getElementById("settingItems"));
+    ReactDOM.render(<div id="previewDiv" />,document.getElementById('textPreview'));
+    showTextPreview('现在预览的是文本框字体大小和播放速度的情况，您可以根据您的观感调整上面的选项。');
+}
+
+function VC_showSave_Load(mode){
+    if(mode === 'save'){
+        ReactDOM.render(<SaveMainModel PageQty={15}/>,document.getElementById('SaveItems'))
+    }else {
+        ReactDOM.render(<LoadMainModel PageQty={15}/>,document.getElementById('LoadItems'))
+    }
+}
+
+function VC_resetStage(){
+    // document.getElementById('mainBackground').style.backgroundImage = 'none';
+    ReactDOM.render(<div/>,document.getElementById('figureImage'));
+    ReactDOM.render(<div/>,document.getElementById('figureImage_left'));
+    ReactDOM.render(<div/>,document.getElementById('figureImage_right'));
+}
+
 function loadBGM() {
     let bgmName = currentInfo["bgm"];
     if(bgmName === '' || bgmName === 'none'){
@@ -90,6 +183,38 @@ function playVocal() {
     let VocalControl = document.getElementById("currentVocal");
     VocalControl.currentTime = 0;
     VocalControl.play();
+}
+
+function ren_miniPic(){
+    document.getElementById('ren_test').style.display = 'block';
+    let backUrl = "./game/background/"+currentInfo["bg_Name"];
+    let leftFigUrl = "./game/figure/"+currentInfo["fig_Name_left"];
+    let FigUrl = "./game/figure/"+currentInfo["fig_Name"];
+    let rightFigUrl = "./game/figure/"+currentInfo["fig_Name_right"];
+    let renderList= [];
+    if(currentInfo["fig_Name_left"]!=='none'&& currentInfo["fig_Name_left"]!==''){
+        let tempIn= <div id={"mini_fig_left"} className={"mini_fig"}>
+            <img src={leftFigUrl} alt={"mini_fig"} className={"mini_fig_pic"}/>
+        </div>
+        renderList.push(tempIn);
+    }
+    if(currentInfo["fig_Name"]!=='none'&& currentInfo["fig_Name"]!==''){
+        let tempIn= <div id={"mini_fig_center"} className={"mini_fig"}>
+            <img src={FigUrl} alt={"mini_fig"} className={"mini_fig_pic"}/>
+        </div>
+        renderList.push(tempIn);
+    }
+    if(currentInfo["fig_Name_right"]!=='none'&& currentInfo["fig_Name_right"]!==''){
+        let tempIn= <div id={"mini_fig_right"} className={"mini_fig"}>
+            <img src={rightFigUrl} alt={"mini_fig"} className={"mini_fig_pic"}/>
+        </div>
+        renderList.push(tempIn);
+    }
+    let element = <div id={"miniPic"}>
+        {renderList}
+    </div>
+    ReactDOM.render(element,document.getElementById('ren_test'));
+    document.getElementById('ren_test').style.backgroundImage = "url('" + backUrl + "')";
 }
 
 function showIntro(text){
@@ -174,5 +299,103 @@ function showTextArray(textArray){
             }
 
         }
+    }
+}
+
+function showTextPreview(text){
+    onTextPreview = onTextPreview+1;
+    let textArray = text.split("");
+    if(Settings["font_size"] === 'small'){
+        document.getElementById('previewDiv').style.fontSize = '150%';
+    }else if(Settings["font_size"] === 'medium'){
+        document.getElementById('previewDiv').style.fontSize = '200%';
+    }else if(Settings["font_size"] === 'large'){
+        document.getElementById('previewDiv').style.fontSize = '250%';
+    }
+    ReactDOM.render(<span> </span>, document.getElementById('previewDiv'));
+    let elementArray = [];
+    let i = 0;
+    clearInterval(interval2);
+    var interval2 = setInterval(showSingle,textShowWatiTime);
+    function showSingle() {
+        if(onTextPreview>1){
+            onTextPreview = onTextPreview-1;
+            clearInterval(interval2);
+            return;
+        }
+        let tempElement = <span key={i} className='singleWord'>{textArray[i]}</span>
+        elementArray.push(tempElement);
+        ReactDOM.render(<div>{elementArray}</div>, document.getElementById('previewDiv'));
+        i = i+1;
+        if(i > textArray.length +(1000/35)){
+            clearInterval(interval2);
+            interval2 = setInterval(showSingle,textShowWatiTime);
+            i = 0;
+            elementArray = [];
+            if(Settings["font_size"] === 'small'){
+                document.getElementById('previewDiv').style.fontSize = '150%';
+            }else if(Settings["font_size"] === 'medium'){
+                document.getElementById('previewDiv').style.fontSize = '200%';
+            }else if(Settings["font_size"] === 'large'){
+                document.getElementById('previewDiv').style.fontSize = '250%';
+            }
+        }
+    }
+}
+
+function showMesModel(Title,Left,Right,func) {
+    document.getElementById('MesModel').style.display='block';
+    let element =
+        <div className={'MesMainWindow'}>
+            <div className={"MesTitle"}>{Title}</div>
+            <div className={'MesChooseContainer'}>
+                <div className={'MesChoose'} onClick={()=>{func();document.getElementById('MesModel').style.display='none';}}>{Left}</div>
+                <div className={'MesChoose'} onClick={()=>{document.getElementById('MesModel').style.display='none';}}>{Right}</div>
+            </div>
+        </div>
+    ReactDOM.render(element,document.getElementById('MesModel'))
+}
+
+function showBacklog(){
+    let even = window.event || arguments.callee.caller.arguments[0];
+    even.preventDefault();
+    even.stopPropagation();//阻止事件冒泡
+    document.getElementById('backlog').style.display = 'block';
+    document.getElementById('bottomBox').style.display = 'none';
+    let showBacklogList = [];
+    for (let i = 0 ; i<CurrentBacklog.length ; i++){
+        let temp = <div className={'backlog_singleElement'} key={i} onClick={()=>{jumpFromBacklog(i)}}>
+            <div className={"backlog_name"}>{CurrentBacklog[i].showName}</div>
+            <div className={"backlog_text"}>{CurrentBacklog[i].showText}</div>
+        </div>
+        showBacklogList.push(temp)
+    }
+    ReactDOM.render(<div>{showBacklogList}</div>,document.getElementById('backlogContent'));
+}
+
+// -------- 紧急回避 --------
+
+function showPanic(showType) {
+    document.querySelector('div#panic-overlay').style.display = 'block';
+    if (showType === 'Yoozle') {
+        let ele =
+            <div className="yoozle-container">
+                <div className="yoozle-title">
+                    <span>
+                        <span className="yoozle-gl-blue">Y</span><span className="yoozle-gl-red">o</span><span
+                        className="yoozle-gl-yellow">o</span><span className="yoozle-gl-blue">z</span><span
+                        className="yoozle-gl-green">l</span><span className="yoozle-gl-red yoozle-e-rotate">e</span>
+                    </span>
+                </div>
+                <div className="yoozle-search">
+                    <input className="yoozle-search-bar" type="text" defaultValue="" />
+                    <div className="yoozle-search-buttons">
+                        <input className="yoozle-btn" type="submit" value="Yoozle Search" />
+                        <input className="yoozle-btn" type="submit" value="I'm Feeling Lucky" />
+                    </div>
+                </div>
+            </div>
+        ReactDOM.render(ele, document.querySelector('div#panic-overlay'));
+        document.querySelector('input.yoozle-search-bar').value = '';
     }
 }
