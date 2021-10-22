492946d33e19a60461d80aede9b0a5c46999085b
MakinoharaShoko <salufthansa@foxmail.com>
2021-10-22 12:46:53 +0800

    WGR 0.1

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {
+    setAutoWaitTime,autoWaitTime,textShowWaitTime,
+    GameInfo,currentScene,auto,fast,onTextPreview,showingText,hideTextStatus,
+    currentInfo,Saves,SaveBacklog,CurrentBacklog,currentSavePage,currentLoadPage,Settings,
+    loadCookie,writeCookie,clearCookie,loadSettings,getStatus,getScene,getGameInfo,SyncCurrentStatus
+} from "./core/storeControl/storeControl"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('ReactRoot')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
