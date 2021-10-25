1d24455b2a32ae6002f7cbee626135efd5952a18
MakinoharaShoko <salufthansa@foxmail.com>
2021-10-25 13:01:17 +0800

    修改UI按钮及动画

--- src/Core/ViewController/ViewControl.js
+++ src/Core/ViewController/ViewControl.js
@@ -16,6 +16,8 @@ import {nextSentenceProcessor} from "../WG_core";
 import React from "react";
 import returnIcon from "../../assests/img/return.svg";
 import vocalIcon from "../../assests/img/vocal.svg"
+import {Return, VolumeNotice} from "@icon-park/react";
+import '@icon-park/react/styles/index.css';
 
 class WG_ViewControl {
     static VC_changeBG(bg_name){
@@ -278,15 +280,27 @@ class WG_ViewControl {
         document.getElementById('bottomBox').style.display = 'none';
         let showBacklogList = [];
         for (let i = 0 ; i<getRuntime().CurrentBacklog.length ; i++){
-            let temp = <div className={'backlog_singleElement'} key={i} onClick={()=>{
-                // userInteract.jumpFromBacklog(i);
-            }}>
+            let temp = <div className={'backlog_singleElement'} key={i}>
                 <div className={"backlog_interact"}>
-                    <div className={"backlog_interact_button"}>
-                        <img src={vocalIcon} alt={"vi"} className={"vocalIcon"}/>
+                    <div className={"backlog_interact_button"} onClick={()=>{
+                        let vocalName = getRuntime().CurrentBacklog[i].vocal;
+                        if(vocalName !== ''){
+                            let url = './game/vocal/'+vocalName;
+                            let elementAudio = <audio src={url} id={"backlogVocalAudio-"+i} />
+                            console.log("Playing! now url is"+url);
+                            ReactDOM.render(elementAudio,document.getElementById("backlogVocal-"+i));
+                            let singleControlBacklogAudio = document.getElementById("backlogVocalAudio-"+i);
+                            singleControlBacklogAudio.oncanplay = function (){
+                                singleControlBacklogAudio.play();
+                            }
+                        }
+                    }}>
+                        <VolumeNotice theme="outline" size="24" fill="#f5f5f7"/>
                     </div>
-                    <div className={"backlog_interact_button"}>
-                        <img src={returnIcon} alt={"ri"} className={"returnIcon"}/>
+                    <div className={"backlog_interact_button"} onClick={()=>{
+                        userInteract.jumpFromBacklog(i);
+                    }}>
+                        <Return theme="outline" size="24" fill="#f5f5f7"/>
                     </div>
                 </div>
                 <div className={"backlog_name"}>{getRuntime().CurrentBacklog[i].showName}</div>
@@ -295,7 +309,9 @@ class WG_ViewControl {
 
                     <div className={"backlog_text"}>{getRuntime().CurrentBacklog[i].showText}</div>
                 </div>
+                <div id={"backlogVocal-"+i}>
 
+                </div>
             </div>
             showBacklogList.push(temp)
         }
