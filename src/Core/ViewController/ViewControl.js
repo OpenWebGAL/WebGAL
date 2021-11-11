9fbd9e9882da6d430f5ba0a206cbbfc4ee98fa8b
MakinoharaShoko <salufthansa@foxmail.com>
2021-11-11 13:28:10 +0800

    修复bgm重叠问题

--- src/Core/ViewController/ViewControl.js
+++ src/Core/ViewController/ViewControl.js
@@ -195,33 +195,41 @@ class WG_ViewControl {
         console.log("loadingBGM")
         let bgmName = getRuntime().currentInfo["bgm"];
         console.log("now playing "+bgmName);
-        console.log(getRuntime().currentInfo);
+        // console.log(getRuntime().currentInfo);
         if(bgmName === '' || bgmName === 'none'){
+            console.log("set bgm none");
+            if(document.getElementById("currentBGM")){
+                document.getElementById("currentBGM").autoplay = false;
+                document.getElementById("currentBGM").pause();
+            }
             ReactDOM.render(<div/>,document.getElementById("bgm"));
             return;
         }
         let url = "./game/bgm/"+bgmName;
         let audio = <audio src={url} id={"currentBGM"} loop="loop"/>
-        console.log("replace old bgm with an empty div")
-        ReactDOM.render(<div/>,document.getElementById("bgm"));
-        ReactDOM.render(audio,document.getElementById("bgm"));
-        let playControl = document.getElementById("currentBGM");
-        let played = false;
-        playControl.oncanplay = function (){
-            if(played === false)
+        // console.log("replace old bgm with an empty div")
+        // ReactDOM.render(<div/>,document.getElementById("bgm"));
+        ReactDOM.render(audio,document.getElementById("bgm"),audioRendered);
+        function audioRendered(){
+            let playControl = document.getElementById("currentBGM");
+            let played = false;
+            console.log(playControl)
+            playControl.oncanplay = function (){
+                if(played === false)
+                {
+                    playControl.currentTime = 0;
+                    playControl.volume = 0.25;
+                    played = true;
+                    playControl.play();
+                }
+            }
+            if(played === false && document.getElementById("currentBGM"))
             {
                 playControl.currentTime = 0;
                 playControl.volume = 0.25;
                 played = true;
+                playControl.play();
             }
-            playControl.play();
-        }
-        if(played === false && document.getElementById("currentBGM"))
-        {
-            playControl.currentTime = 0;
-            playControl.volume = 0.25;
-            played = true;
-            playControl.play();
         }
     }
 
