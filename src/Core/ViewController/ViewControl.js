fcdd6c2bd75e8746e7aaaec3737101d19da80468
MakinoharaShoko <salufthansa@foxmail.com>
2021-11-20 23:27:14 +0800

    Revert "Merge branch 'Dev_App_Version' of https://github.com/MakinoharaShoko/WebGAL into Dev_App_Version"
    This reverts commit ea23f321e6763e308204c1f500d18d3ff4cb0026, reversing
    changes made to 677fb3e7d8aa5f42d386da58d8ebb5043e7edaa1.

--- src/Core/ViewController/ViewControl.js
+++ src/Core/ViewController/ViewControl.js
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom";
-import {clearCookie, getRuntime, getStatus, SyncCurrentStatus} from "../StoreControl/StoreControl";
+import {
+CurrentBacklog, SyncCurrentStatus, getStatus, clearCookie, fast, getRuntime
+} from "../StoreControl/StoreControl";
 
 import {userInteract} from "../InteractController/UserInteract";
 import {
@@ -12,9 +14,10 @@ import {
 } from "../../Components/UI/etc";
 import {nextSentenceProcessor} from "../WG_core";
 import React from "react";
+import returnIcon from "../../assests/img/return.svg";
+import vocalIcon from "../../assests/img/vocal.svg"
 import {Return, VolumeNotice} from "@icon-park/react";
 import '@icon-park/react/styles/index.css';
-import AudioController from "../util/AudioController.js";
 
 class WG_ViewControl {
     static VC_changeBG(bg_name){
@@ -210,19 +213,66 @@ class WG_ViewControl {
     }
 
     static loadBGM() {
+        console.log("loadingBGM")
         let bgmName = getRuntime().currentInfo["bgm"];
-
-        let bgmPath = bgmName === '' || bgmName === 'none' ? null : `./game/bgm/${bgmName}`
-
-        AudioController.loadAudioFile(AudioController.MAIN_BGM, bgmPath, true);
+        console.log("now playing "+bgmName);
+        // console.log(getRuntime().currentInfo);
+        if(bgmName === '' || bgmName === 'none'){
+            console.log("set bgm none");
+            if(document.getElementById("currentBGM")){
+                document.getElementById("currentBGM").autoplay = false;
+                document.getElementById("currentBGM").pause();
+            }
+            ReactDOM.render(<div/>,document.getElementById("bgm"));
+            return;
+        }
+        let url = "./game/bgm/"+bgmName;
+        let audio = <audio src={url} id={"currentBGM"} loop="loop"/>
+        // console.log("replace old bgm with an empty div")
+        // ReactDOM.render(<div/>,document.getElementById("bgm"));
+        ReactDOM.render(audio,document.getElementById("bgm"),audioRendered);
+        function audioRendered(){
+            let playControl = document.getElementById("currentBGM");
+            let played = false;
+            console.log(playControl)
+            playControl.oncanplay = function (){
+                if(played === false)
+                {
+                    playControl.currentTime = 0;
+                    playControl.volume = 0.25;
+                    played = true;
+                    playControl.play();
+                }
+            }
+            if(played === false && document.getElementById("currentBGM"))
+            {
+                playControl.currentTime = 0;
+                playControl.volume = 0.25;
+                played = true;
+                playControl.play();
+            }
+        }
     }
 
     static playVocal() {
+        console.log("Playing vocal:")
         let runtimeTemp = getStatus("all")
+        if(document.getElementById('currentVocal')){
+            document.getElementById('currentVocal').pause();
+        }
         let vocalName = runtimeTemp.vocal;
-        let vocalPath = vocalName === '' ? null : `./game/vocal/${vocalName}`
-
-        AudioController.loadAudioFile(AudioController.VOCAL, vocalPath, true);
+        if(vocalName === ''){
+            ReactDOM.render(<div/>,document.getElementById('vocal'));
+            return;
+        }
+        let url = './game/vocal/'+vocalName;
+        let vocal = <audio src={url} id={"currentVocal"}/>
+        ReactDOM.render(vocal,document.getElementById('vocal'));
+        let VocalControl = document.getElementById("currentVocal");
+        VocalControl.currentTime = 0;
+        VocalControl.oncanplay = function (){
+            VocalControl.play();
+        }
     }
 
     static showIntro(text){
@@ -278,11 +328,30 @@ class WG_ViewControl {
         for (let i = 0 ; i<getRuntime().CurrentBacklog.length ; i++){
             let temp = <div className={'backlog_singleElement'} key={i}>
                 <div className={"backlog_interact"}>
-                    <div className={"backlog_interact_button"} onClick={()=> {
+                    <div className={"backlog_interact_button"} onClick={()=>{
                         let vocalName = getRuntime().CurrentBacklog[i].vocal;
-                        let vocalPath = vocalName === '' ? null : `./game/vocal/${vocalName}`
-
-                        AudioController.loadAudioFile(AudioController.VOCAL, vocalPath, true);
+                        if(vocalName !== ''){
+                            let url = './game/vocal/'+vocalName;
+                            let elementAudio = <audio src={url} id={"backlogVocalAudio-"+i} />
+                            console.log("Playing! now url is"+url);
+                            ReactDOM.render(elementAudio,document.getElementById("backlogVocal-"+i));
+                            let singleControlBacklogAudio = document.getElementById("backlogVocalAudio-"+i);
+                            let played = false;
+                            played = false;
+                            singleControlBacklogAudio.oncanplay = function (){
+                                if(!played){
+                                    singleControlBacklogAudio.currentTime = 0;
+                                    played = true;
+                                    singleControlBacklogAudio.play();
+                                }
+                                singleControlBacklogAudio.play();
+                            }
+                            if(!played){
+                                singleControlBacklogAudio.currentTime = 0;
+                                played = true;
+                                singleControlBacklogAudio.play();
+                            }
+                        }
                     }}>
                         <VolumeNotice theme="outline" size="24" fill="#f5f5f7"/>
                     </div>
@@ -342,12 +411,11 @@ class WG_ViewControl {
             if(i > textArray.length && getRuntime().auto !== 1){
                 getRuntime().showingText = false;
             }
-            if (i > textArray.length + (getRuntime().autoWaitTime / 35)) {
-                if (getRuntime().auto === 1) {
-                    // if(document.getElementById('currentVocal') && getRuntime().fast === 0){
-                    //     if(document.getElementById('currentVocal').ended)
-                    if (getRuntime().fast === 0) {
-                        if (AudioController.isSourceEnded(AudioController.VOCAL)) {
+            if(i > textArray.length +(getRuntime().autoWaitTime/35)){
+                if(getRuntime().auto === 1){
+                    if(document.getElementById('currentVocal') && getRuntime().fast === 0){
+                        if(document.getElementById('currentVocal').ended)
+                        {
                             clearInterval(interval);
                             getRuntime().showingText = false;
                             nextSentenceProcessor();
