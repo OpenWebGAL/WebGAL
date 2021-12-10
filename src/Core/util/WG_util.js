5217bdf8ef574bf89846de75ecf61b1e6fe38577
Kamome <makinoharashoko@outlook.com>
2021-12-10 15:02:18 +0800

    优化手机的竖屏显示（强制旋转到横屏）

--- src/Core/util/WG_util.js
+++ src/Core/util/WG_util.js
@@ -128,6 +128,44 @@ function MobileChangeStyle(){
     document.getElementById('ReactRoot').style.fontSize = '65%';
     document.getElementById('mainTextWindow').style.padding = '5px 20% 5px 20%';
     document.getElementById('SceneText').style.padding = '5px 0 0 0';
+    forceRotate();
+}
+
+function forceRotate(){
+    const width = document.documentElement.clientWidth;
+    const height = document.documentElement.clientHeight;
+    var screen_width = width; //屏幕宽度
+    const contentDOM = document.getElementById('ReactRoot');
+    if (width < height) {
+        screen_width = height; //如果 是竖屏，灵感的宽度就等于屏高
+        contentDOM.style.width = height + 'px';
+        contentDOM.style.height = width + 'px';
+        contentDOM.style.position = 'relative'
+        contentDOM.style.top = (height - width) / 2 + 'px';
+        contentDOM.style.left = 0 - (height - width) / 2 + 'px';
+        contentDOM.style.transform = 'rotate(90deg)';
+    }
+//根据手机旋转的角度来判断
+    const evt = "onorientationchange" in window ? "orientationchange" : "resize"; //旋转事件
+    window.addEventListener(evt, function () { //事件监听
+        if (window.orientation === 90 || window.orientation === -90) { //旋转到 90 或 -90 度，即竖屏到横屏
+            screen_width = height; //横屏，灵感的宽度就等于屏高
+            contentDOM.style.width = height + 'px';
+            contentDOM.style.height = width + 'px';
+            contentDOM.style.position = 'relative'
+            contentDOM.style.top = '0px';
+            contentDOM.style.left = '0px';
+            contentDOM.style.transform = 'none'; //不旋转；
+        }else{ //旋转到 180 或 0 度，即横屏到竖屏
+            screen_width = height; //竖屏，灵感的宽度就等于屏高
+            contentDOM.style.width = height + 'px';
+            contentDOM.style.height = width + 'px';
+            contentDOM.style.position = 'relative'
+            contentDOM.style.top = (height - width) / 2 + 'px';
+            contentDOM.style.left = 0 - (height - width) / 2 + 'px';
+            contentDOM.style.transform = 'rotate(90deg)'; //旋转90度
+        }
+    }, false);
 }
 
 export {processSentence,queryWidgetState,loadSettings,AllHiddenIgnore,isMobile,MobileChangeStyle}
