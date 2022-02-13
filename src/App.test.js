c6b52ce7f5ef199a7fd8cef1c041f217adde6ce3
MakinoharaShoko <salufthansa@foxmail.com>
2022-02-13 11:45:10 +0800

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('testTitle', () => {
   render(<App />);
   const linkElement = screen.getByText(/START/i);
   expect(linkElement).toBeInTheDocument();
