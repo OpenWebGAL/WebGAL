4a4f2b0fcbb06160f4d4b037c22232a317a939da
MakinoharaShoko <salufthansa@foxmail.com>
2022-02-15 17:41:59 +0800

    加入新的下雨特效

--- src/Core/PixiController/PixiControl.js
+++ src/Core/PixiController/PixiControl.js
@@ -1,10 +1,11 @@
 import pixiRain from "./presets/rain";
 import pixiSnow from "./presets/snow";
+import pixiRain2 from "./presets/rain2";
 
 const PixiControl = (performType, option) => {
     switch (performType){
         case 'rain':
-            pixiRain(3);
+            pixiRain2(3,10);
             break;
         case 'snow':
             pixiSnow(3);
