a1a4778a77020f7923b57d223179e83a725a8e7e
MakinoharaShoko <salufthansa@foxmail.com>
2022-02-16 08:49:49 +0800

    为特效层的点击绑定事件

--- src/Components/Stage/assetsContainer.js
+++ src/Components/Stage/assetsContainer.js
@@ -1,7 +1,7 @@
 import {WG_ViewControl} from "../../Core/ViewController/ViewControl";
 import {userInteract} from "../../Core/InteractController/UserInteract";
 
-const AssetsContainer = ()=>{
+const AssetsContainer = () => {
     return <>
         <div id="intro" className="intro_styl"/>
         <div id={"videoContainer"} className={"videoContainer_styl"} onClick={WG_ViewControl.closeVideo}/>
@@ -13,7 +13,7 @@ const AssetsContainer = ()=>{
         <div id="figureImage" className={"figureContainercenter"} onClick={userInteract.clickOnBack}/>
         <div id="figureImage_left" className={"figureContainerleft"} onClick={userInteract.clickOnBack}/>
         <div id="figureImage_right" className={"figureContainerright"} onClick={userInteract.clickOnBack}/>
-        <div id={'pixiContianer'}/>
+        <div id={'pixiContianer'} onClick={userInteract.clickOnBack}/>
         <div id="chooseBox"/>
         <div id="bgm"/>
         <div id="vocal"/>
