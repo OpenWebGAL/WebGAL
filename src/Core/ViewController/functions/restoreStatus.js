eb6da0d68c42c894bce36bfc178dcd3e3e954fbe
MakinoharaShoko <salufthansa@foxmail.com>
2022-02-17 21:03:47 +0800

    修复恢复状态时背景的问题

--- src/Core/ViewController/functions/restoreStatus.js
+++ src/Core/ViewController/functions/restoreStatus.js
@@ -5,13 +5,17 @@ import {processSelection} from "../../util/WG_util";
 const restoreStatus = (savedStatus) => {
     const command = savedStatus["command"];
     //恢复对话
-    WG_ViewControl.VC_textShow(savedStatus['showName'],savedStatus['showText']);
+    WG_ViewControl.VC_textShow(savedStatus['showName'], savedStatus['showText']);
     //恢复背景的移动和效果
     if (savedStatus['bg_transform'] !== '') {
         document.getElementById('mainBackground').style.transform = savedStatus['bg_transform'];
+    } else {
+        document.getElementById('mainBackground').style.transform = '';
     }
     if (savedStatus['bg_filter'] !== '') {
         document.getElementById('mainBackground').style.filter = savedStatus['bg_filter'];
+    } else {
+        document.getElementById('mainBackground').style.filter = '';
     }
 
     //不重置设置背景
@@ -19,14 +23,14 @@ const restoreStatus = (savedStatus) => {
         document.getElementById('mainBackground').style.backgroundImage = "url('game/background/" + savedStatus["bg_Name"] + "')";
 
     // 处理立绘
-    WG_ViewControl.VC_changeP(savedStatus["fig_Name_left"],'left');
-    WG_ViewControl.VC_changeP(savedStatus["fig_Name"],'center');
-    WG_ViewControl.VC_changeP(savedStatus["fig_Name_right"],'right');
+    WG_ViewControl.VC_changeP(savedStatus["fig_Name_left"], 'left');
+    WG_ViewControl.VC_changeP(savedStatus["fig_Name"], 'center');
+    WG_ViewControl.VC_changeP(savedStatus["fig_Name_right"], 'right');
 
     // 处理choose
     if (command === 'choose' || command === 'choose_label') {
         const selection = processSelection(savedStatus["choose"]);
-        const choose_mode = command==='choose'?'scene':'label';
+        const choose_mode = command === 'choose' ? 'scene' : 'label';
         WG_ViewControl.VC_choose(selection, choose_mode);
     }
     // 处理小头像
@@ -42,8 +46,8 @@ const restoreStatus = (savedStatus) => {
 
     //还原演出效果
     WG_ViewControl.VC_PIXI_Create();
-    for(const perform of savedStatus.pixiPerformList){
-        WG_ViewControl.VC_PIXI_perform(perform.performType,perform.option);
+    for (const perform of savedStatus.pixiPerformList) {
+        WG_ViewControl.VC_PIXI_perform(perform.performType, perform.option);
     }
 }
 
