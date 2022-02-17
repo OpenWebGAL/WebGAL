2ccc086059dc948b7a597e280018e323421d00ca
MakinoharaShoko <salufthansa@foxmail.com>
2022-02-17 12:58:44 +0800

    添加关于信息的log

--- src/index.js
+++ src/index.js
@@ -37,10 +37,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
+getGameInfo();
 loadStorage();
 loadSettings();
-getGameInfo();
-
 if (isMobile())
     MobileChangeStyle();
