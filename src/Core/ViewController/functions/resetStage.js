6c8daca8157d69ee8f1a6d0c8a0bd8092c4f4cde
MakinoharaShoko <salufthansa@foxmail.com>
2022-03-04 09:22:41 +0800

    修复重新开始后场景没有清空的问题

--- src/Core/ViewController/functions/resetStage.js
+++ src/Core/ViewController/functions/resetStage.js
@@ -2,7 +2,7 @@ import restoreStatus from "./restoreStatus";
 import {initcurrentInfo} from "../../StoreControl/StoreControl";
 
 const resetStage = () => {
-    restoreStatus(JSON.parse(JSON.stringify(initcurrentInfo)));
+    restoreStatus(JSON.parse(JSON.stringify(initcurrentInfo)),true);
 }
 
 export default resetStage;
