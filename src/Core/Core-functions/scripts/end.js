c0c0136e87e3a14b903e89d7b8b70647ebd53250
MakinoharaShoko <salufthansa@foxmail.com>
2022-03-04 08:44:29 +0800

    修复重置场景导致的首行文本不显示问题

--- src/Core/Core-functions/scripts/end.js
+++ src/Core/Core-functions/scripts/end.js
@@ -6,6 +6,8 @@ import logger from "../../util/logger";
 const gameEnd = () => {
     //首先初始化状态列表
     logger.debug('初始化场景',initcurrentInfo)
+    //重置场景
+    WG_ViewControl.VC_resetStage();
     getRuntime().CurrentBacklog = [];
     getRuntime().currentScene = '';
     WG_ViewControl.VC_resetStage();
