1d83653da83b16f0e6dd8a2115bbbc70496f9f17
MakinoharaShoko <salufthansa@foxmail.com>
2022-03-13 16:40:11 +0800

    4.X init

--- src/Core/Core-functions/scripts/intro.js
+++
@@ -1,8 +0,0 @@
-import {WG_ViewControl} from "../../ViewController/ViewControl";
-
-const intro = (S_content) => {
-    WG_ViewControl.showIntro(S_content);
-    return {'ret': true, 'autoPlay': false};
-}
-
-export default intro;
--- src/Core/ViewController/functions/resetStage.js
+++
@@ -1,8 +0,0 @@
-import restoreStatus from "./restoreStatus";
-import {initcurrentInfo} from "../../StoreControl/StoreControl";
-
-const resetStage = () => {
-    restoreStatus(JSON.parse(JSON.stringify(initcurrentInfo)),true);
-}
-
-export default resetStage;
