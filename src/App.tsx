f7c671f59287fbd1517cd4fe1965cec608b7b9a8
Mahiru <makinoharashoko@outlook.com>
2022-03-16 22:57:41 +0800

    准备加上全局变量管理类

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import { Provider } from "reto/provider";
+import {Provider} from "reto/provider";
 import {GuiStateStore} from "./Core/store/GUI";
 import Title from "./Components/UI/Title";
+import {useEffect, useRef} from "react";
+import {storeGlobal} from "./Core/store/storeRef";
 
 function App() {
-  return (
-    <div className="App" style={{height:'100%',width:'100%'}}>
-      <Provider of={GuiStateStore}>
-        <Title/>
-      </Provider>
-    </div>
-  )
+    const GuiStoreRef = useRef<ReturnType<any>>();
+    useEffect(() => {
+        storeGlobal.GUI = GuiStoreRef;
+    }, [])
+    return (
+        <div className="App" style={{height: '100%', width: '100%'}}>
+            <Provider of={GuiStateStore} storeRef={GuiStoreRef}>
+                <Title/>
+            </Provider>
+        </div>
+    )
 }
 
 export default App
---
+++ src/Core/store/storeRef.ts
@@ -0,0 +1,8 @@
+interface storeRef{
+    GUI:any
+}
+
+
+export const storeGlobal:storeRef = {
+    GUI:undefined,
+}
