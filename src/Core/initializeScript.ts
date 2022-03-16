16385509810419968a3683af805028545c71d458
Mahiru <makinoharashoko@outlook.com>
2022-03-16 22:07:42 +0800

    准备换成redux

--- src/Core/initializeScript.ts
+++ src/Core/initializeScript.ts
@@ -3,13 +3,14 @@
  * 引擎初始化时会执行的脚本，包括获取游戏信息，初始化运行时变量，初始化用户数据存储
  */
 
-import { logger } from "../util/logger";
+import {infoFetcher} from "./assetsFetcher/infoFetcher";
+import {logger} from "./util/logger";
 
-export const initializeScript:()=>void= ():void =>{
+export const initializeScript: () => void = (): void => {
     //打印初始log信息
     logger.info('WebGAL 3.9.7');
     logger.info('Github: https://github.com/MakinoharaShoko/WebGAL ');
     logger.info('Made with ❤ by MakinoharaShoko');
-
+    infoFetcher('./game/config.txt');
 
 }
