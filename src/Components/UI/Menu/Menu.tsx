2e08655eb18c74cab3d01848c23acdd8f6b973b4
Mahiru <makinoharashoko@outlook.com>
2022-03-17 10:46:52 +0800

    Update Menu.tsx

--- src/Components/UI/Menu/Menu.tsx
+++ src/Components/UI/Menu/Menu.tsx
@@ -1,12 +1,13 @@
 import {FC} from "react";
 import {useStore} from "reto";
 import {GuiStateStore} from "../../../Core/store/GUI";
+import styles from './menu.module.scss'
 
 const Menu: FC = () => {
     const GuiStore = useStore(GuiStateStore);
     return <>
         {GuiStore.GuiState.showMenuPanel &&
-            <div>
+            <div className={styles.Menu_main}>
                 Menu
             </div>}
     </>
