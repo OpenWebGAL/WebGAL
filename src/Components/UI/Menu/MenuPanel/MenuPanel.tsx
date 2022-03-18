0c31044eeb35e5a6a2fdd8f6aee149150e50f3de
Mahiru <makinoharashoko@outlook.com>
2022-03-18 10:04:05 +0800

    解耦Menu选项卡

--- src/Components/UI/Menu/MenuPanel/MenuPanel.tsx
+++ src/Components/UI/Menu/MenuPanel/MenuPanel.tsx
@@ -2,6 +2,7 @@ import styles from './menuPanel.module.scss'
 import {useStore} from "reto";
 import {GuiStateStore, MenuPanelTag} from "../../../../Core/store/GUI";
 import {Save, SettingTwo, FolderOpen, Home, Logout} from "@icon-park/react";
+import {MenuPanelButton} from "./MenuPanelButton";
 
 export const MenuPanel = () => {
     const GuiStore = useStore(GuiStateStore);
@@ -16,60 +17,30 @@ export const MenuPanel = () => {
     const OptionTagColor = GuiStore.GuiState.currentMenuTag === MenuPanelTag.Option ? `rgba(81, 110, 65, 0.9)` : `rgba(8, 8, 8, 0.3)`;
 
     //设置Menu图标的颜色
-    const SaveIconColor = GuiStore.GuiState.currentMenuTag === MenuPanelTag.Save ? `rgba(74, 34, 93, 0.9)` : `rgba(185,185,185,1)`;
-    const LoadIconColor = GuiStore.GuiState.currentMenuTag === MenuPanelTag.Load ? `rgba(11, 52, 110, 0.9)` : `rgba(185,185,185,1)`;
-    const OptionIconColor = GuiStore.GuiState.currentMenuTag === MenuPanelTag.Option ? `rgba(81, 110, 65, 0.9)` : `rgba(185,185,185,1)`;
+    const SaveIconColor = GuiStore.GuiState.currentMenuTag === MenuPanelTag.Save ? `rgba(74, 34, 93, 0.9)` : `rgba(185, 185, 185, 1)`;
+    const LoadIconColor = GuiStore.GuiState.currentMenuTag === MenuPanelTag.Load ? `rgba(11, 52, 110, 0.9)` : `rgba(185, 185, 185, 1)`;
+    const OptionIconColor = GuiStore.GuiState.currentMenuTag === MenuPanelTag.Option ? `rgba(81, 110, 65, 0.9)` : `rgba(185, 185, 185, 1)`;
 
     return <div className={styles.MenuPanel_main}>
-        <div className={styles.MenuPanel_button + SaveTagOn}
-             onClick={() => {
-                 GuiStore.setMenuPanelTag(MenuPanelTag.Save)
-             }}
-             style={{color: SaveTagColor}}
-        >
-            <div className={styles.MenuPanel_button_icon}>
-                <Save theme="outline" size="1.2em" fill={SaveIconColor}
-                      strokeWidth={2}/>
-            </div>
-            存档
-        </div>
-        <div className={styles.MenuPanel_button + LoadTagOn}
-             onClick={() => {
-                 GuiStore.setMenuPanelTag(MenuPanelTag.Load)
-             }}
-             style={{color: LoadTagColor}}
-        >
-            <div className={styles.MenuPanel_button_icon}>
-                <FolderOpen theme="outline" size="1.2em" fill={LoadIconColor}
-                            strokeWidth={2}/>
-            </div>
-            读档
-        </div>
-        <div className={styles.MenuPanel_button + OptionTagOn}
-             onClick={() => {
-                 GuiStore.setMenuPanelTag(MenuPanelTag.Option)
-             }}
-             style={{color: OptionTagColor}}
-        >
-            <div className={styles.MenuPanel_button_icon}>
-                <SettingTwo theme="outline" size="1.2em" fill={OptionIconColor}
-                            strokeWidth={2}/>
-            </div>
-            选项
-        </div>
-        <div className={styles.MenuPanel_button}>
-            <div className={styles.MenuPanel_button_icon}>
-                <Home theme="outline" size="1.2em" fill={'rgba(185,185,185,1)'}
-                      strokeWidth={2}/>
-            </div>
-            标题
-        </div>
-        <div className={styles.MenuPanel_button}>
-            <div className={styles.MenuPanel_button_icon}>
-                <Logout theme="outline" size="1.2em" fill={'rgba(185,185,185,1)'}
-                      strokeWidth={2}/>
-            </div>
-            结束
-        </div>
+        <MenuPanelButton iconName={'save'} buttonOnClassName={SaveTagOn} iconColor={SaveIconColor}
+                         tagColor={SaveTagColor}
+                         clickFunc={() => {
+                             GuiStore.setMenuPanelTag(MenuPanelTag.Save)
+                         }}
+                         tagName={'存档'} key={'saveButton'}/>
+        <MenuPanelButton iconName={'load'} buttonOnClassName={LoadTagOn} iconColor={LoadIconColor}
+                         tagColor={LoadTagColor}
+                         clickFunc={() => {
+                             GuiStore.setMenuPanelTag(MenuPanelTag.Load)
+                         }}
+                         tagName={'读档'} key={'loadButton'}/>
+        <MenuPanelButton iconName={'option'} buttonOnClassName={OptionTagOn} iconColor={OptionIconColor}
+                         tagColor={OptionTagColor}
+                         clickFunc={() => {
+                             GuiStore.setMenuPanelTag(MenuPanelTag.Option)
+                         }}
+                         tagName={'选项'} key={'optionButton'}/>
+        <MenuPanelButton iconName={'title'} tagName={'标题'} key={'titleIcon'}/>
+        <MenuPanelButton iconName={'exit'} tagName={'结束'} key={'exitIcon'}/>
     </div>
 }
---
+++ src/Components/UI/Menu/MenuPanel/menuPanelInterface.ts
@@ -0,0 +1,12 @@
+/**
+ * @interface
+ * Menu页面的按钮的参数接口
+ */
+export interface IMenuPanel {
+    clickFunc?: any,
+    buttonOnClassName?: string,
+    tagColor?: string,
+    iconColor?: string,
+    tagName?:string,
+    iconName: string,
+}
