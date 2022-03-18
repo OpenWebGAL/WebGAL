e60827a3e7089da6884c115c1c1026e189850ec5
Mahiru <makinoharashoko@outlook.com>
2022-03-18 11:06:02 +0800

    准备Menu各页面的UI主体

--- src/Components/UI/Menu/Options/Options.tsx
+++ src/Components/UI/Menu/Options/Options.tsx
@@ -1,7 +1,12 @@
 import {FC} from "react";
+import styles from './options.module.scss'
 
 export const Options: FC = () => {
-    return <div>
-        Options
+    return <div className={styles.Options_main}>
+        <div className={styles.Options_top}>
+            <div className={styles.Options_title}>
+                <div className={styles.Option_title_text}>选项</div>
+            </div>
+        </div>
     </div>
 }
--- src/Components/UI/Menu/Save/Save.tsx
+++ src/Components/UI/Menu/Save/Save.tsx
@@ -1,7 +1,14 @@
 import {FC} from "react";
+import styles from "./save.module.scss";
 
-export const Save:FC = () => {
-    return <div>
-        Save
+export const Save: FC = () => {
+    return <div className={styles.Save_main}>
+        <div className={styles.Save_top}>
+            <div className={styles.Save_title}>
+                <div className={styles.Save_title_text}>
+                    存档
+                </div>
+            </div>
+        </div>
     </div>
 }
