9e3478709ab3bb1da4ba6e1b3aae0463948b1b74
Mahiru <makinoharashoko@outlook.com>
2022-03-19 19:13:00 +0800

    更新语句解析器草案

--- src/Core/parser/sceneParser.ts
+++ src/Core/parser/sceneParser.ts
@@ -11,11 +11,13 @@ import {scenePrefetcher} from "../util/scenePrefetcher";
  */
 export const sceneParser = (rawScene: string, sceneName: string): IScene => {
     const rawSentenceList = rawScene.split('\n');//原始句子列表
-    const assetsList: Array<IAsset> = [];//场景资源列表
-    const subSceneList: Array<string> = [];//子场景列表
+    let assetsList: Array<IAsset> = [];//场景资源列表
+    let subSceneList: Array<string> = [];//子场景列表
     const sentenceList: Array<ISentence> = rawSentenceList.map(sentence => {
         const returnSentence: ISentence = scriptParser(sentence);
-        //TODO: 在这里解析出语句可能携带的资源和场景，push 到 assetsList 和 subSceneList
+        //在这里解析出语句可能携带的资源和场景，合并到 assetsList 和 subSceneList
+        assetsList = [...assetsList, ...returnSentence.sentenceAssets];
+        subSceneList = [...subSceneList, returnSentence.subScene];
         return returnSentence;
     });
     //开始资源的预加载
