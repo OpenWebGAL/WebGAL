9e9ed1752de9717f898614d2b935c2dce8408371
MakinoharaShoko <salufthansa@foxmail.com>
2022-03-19 12:08:20 +0800

    准备场景解析的接口

--- src/main.tsx
+++ src/main.tsx
@@ -8,18 +8,4 @@ ReactDOM.render(
     <React.StrictMode>
         <App/>
     </React.StrictMode>,
-    document.getElementById('root'), () => {
-        //在调整窗口大小时重新计算宽高，设计稿按照 1600*900。
-        setTimeout(resize, 100)
-        resize();
-        window.onresize = resize;
-        document.onkeydown = function (event) {
-            const e = event || window.event || arguments.callee.caller.arguments[0];
-            if (e && e.keyCode == 122) {
-                setTimeout(() => {
-                    resize();
-                }, 100);
-            }
-        };
-    }
-)
+    document.getElementById('root'));
