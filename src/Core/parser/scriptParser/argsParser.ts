69f0ecd1c20ea40fb403e72cb95be26ff71c81c2
Mahiru <makinoharashoko@outlook.com>
2022-03-20 11:19:54 +0800

    完成语句解析与参数解析示范

--- src/Core/parser/scriptParser/argsParser.ts
+++ src/Core/parser/scriptParser/argsParser.ts
@@ -1,12 +1,59 @@
 import {arg} from "../../interface/scene";
+import {logger} from "../../util/logger";
+import {assetSetter, fileType} from "../../util/assetSetter";
 
-const argsParser = (argsRaw: string): Array<arg> => {
+export const argsParser = (argsRaw: string): Array<arg> => {
     const returnArrayList: Array<arg> = [];
     //处理参数
+    //去空格
+    argsRaw = argsRaw.replace(/ /g, '');
+    logger.info('去完空格', argsRaw);
     // 分割参数列表
     let rawArgsList: Array<string> = argsRaw.split('-');
-    //去空格
-    rawArgsList = rawArgsList.map(e => e.replace(/ /g, ''));
-    
+    //去除空字符串
+    rawArgsList = rawArgsList.filter(e => {
+        return e !== ''
+    });
+    logger.info('分割完后', rawArgsList);
+    rawArgsList.forEach(e => {
+        const argName = e.split('=')[0];
+        const argValue = e.split('=')[1];
+        //判断是不是语音参数
+        if (e.match(/.ogg|.mp3|.wav/)) {
+            returnArrayList.push({
+                key: 'vocal',
+                value: assetSetter(e, fileType.vocal)
+            })
+        } else {
+            //判断是不是省略参数
+            if (argValue === undefined) {
+                returnArrayList.push({
+                    key: argName,
+                    value: true
+                })
+            } else {
+                //是字符串描述的布尔值
+                if (argValue === 'true' || argValue === 'false') {
+                    returnArrayList.push({
+                        key: argName,
+                        value: argValue === 'true'
+                    })
+                } else {
+                    //是数字
+                    if (!isNaN(Number(argValue))) {
+                        returnArrayList.push({
+                            key: argName,
+                            value: argValue
+                        })
+                    } else {//是普通参数
+                        returnArrayList.push({
+                            key: argName,
+                            value: argValue
+                        })
+                    }
+                }
+            }
+        }
+    })
     return returnArrayList;
 }
