657508e784de74bd67d97addabaac7b604b7206e
MakinoharaShoko <salufthansa@foxmail.com>
2022-03-20 22:51:03 +0800

    Update perform.ts

--- src/Core/interface/perform.ts
+++ src/Core/interface/perform.ts
@@ -5,7 +5,7 @@
 export interface IPerform {
     performName: string,// 演出名称
     duration: number, // 持续时间，单位为ms
-    stopFunction: any, // 结束演出的函数
-    interruptable : boolean, //演出是否可以被打断
-    blockNext:boolean // 演出是否阻塞游戏流程继续
+    stopFunction: Function, // 结束演出的函数
+    interruptable: boolean, //演出是否可以被打断
+    blockNext: Function // 演出是否阻塞游戏流程继续（一个函数，返回 boolean类型的结果，判断要不要阻塞）
 }
