6f0ec24a8f71cf6378ef4728978d55f63e4cc73b
MakinoharaShoko <salufthansa@foxmail.com>
2022-03-20 23:09:10 +0800

    Update perform.ts

--- src/Core/interface/perform.ts
+++ src/Core/interface/perform.ts
@@ -7,9 +7,9 @@ export interface IPerform {
     duration: number, // 持续时间，单位为ms
     isHoldOn: boolean, //演出要不要进舞台状态，在读档或回跳时触发，这时这个演出会保持，只能被专门的清除语句清除。
     stopFunction: Function, // 结束演出的函数，在演出时间到或被fast跳过后会触发（setTimeout(stopFunction,duration)用来在演出结束后清除演出）
-    interruptable: Function, //演出是否可以被打断（一个函数，返回 boolean类型的结果，判断能不能打断）
+    interruptible: Function, //演出是否可以被打断（一个函数，返回 boolean类型的结果，判断能不能打断）
     blockNext: Function // 演出是否阻塞游戏流程继续（一个函数，返回 boolean类型的结果，判断要不要阻塞）
-    blockAuto:Function //演出是否阻塞自动模式（一个函数，返回 boolean类型的结果，判断要不要阻塞）
+    blockAuto: Function //演出是否阻塞自动模式（一个函数，返回 boolean类型的结果，判断要不要阻塞）
 }
 
 //next之后，可以被打断的演出会被打断，不能被打断的演出会继续，阻塞next的演出会阻止next被响应。
