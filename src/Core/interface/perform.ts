6ccd41e5f98fe9b33215e0b20d271b22399b9e8b
MakinoharaShoko <salufthansa@foxmail.com>
2022-03-20 22:46:21 +0800

    Update perform.ts

--- src/Core/interface/perform.ts
+++ src/Core/interface/perform.ts
@@ -6,5 +6,6 @@ export interface IPerform {
     performName: string,// 演出名称
     duration: number, // 持续时间，单位为ms
     stopFunction: any, // 结束演出的函数
-    interruptable : boolean //演出是否可以被打断
+    interruptable : boolean, //演出是否可以被打断
+    blockNext:boolean // 演出是否阻塞游戏流程继续
 }
