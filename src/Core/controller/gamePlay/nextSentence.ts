b72c1dfcc4ed8d8dfacdcc8af05ba49443ce2271
Mahiru <makinoharashoko@outlook.com>
2022-03-21 15:01:22 +0800

    加上演出管理器和next的基本逻辑

--- src/Core/controller/gamePlay/nextSentence.ts
+++ src/Core/controller/gamePlay/nextSentence.ts
@@ -1,3 +1,11 @@
-export const nextSentence = () => {
+import {scriptExecutor} from "./scriptExecutor";
 
+export const nextSentence = () => {
+    //检查是否存在blockNext 的演出
+    const isBlockingNext = false;
+    if (isBlockingNext) {
+        return;
+    } else {
+        scriptExecutor();
+    }
 }
--- src/Core/runtime/sceneData.ts
+++ src/Core/runtime/sceneData.ts
@@ -6,17 +6,19 @@ import {IScene} from "../interface/scene";
  * @interface ISceneData
  */
 interface ISceneData {
+    currentSentenceId: number,//当前语句ID
     sceneStack: Array<sceneEntry>, //场景栈
     currentScene: IScene, //当前场景数据
 }
 
 //场景数据
 export const runtime_currentSceneData: ISceneData = {
+    currentSentenceId: 0,//当前语句ID
     sceneStack: [],
     //初始场景，没有数据
     currentScene: {
         sceneName: '', //场景名称
-        sceneUrl:'', //场景url
+        sceneUrl: '', //场景url
         sentenceList: [], //语句列表
         assetsList: [], //资源列表
         subSceneList: [] //子场景列表
