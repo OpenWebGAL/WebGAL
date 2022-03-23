fef3eb2903cf2b3996605e1e66bc5ab9e228919d
Mahiru <makinoharashoko@outlook.com>
2022-03-23 21:48:49 +0800

    Update nextSentence.ts

--- src/Core/controller/gamePlay/nextSentence.ts
+++ src/Core/controller/gamePlay/nextSentence.ts
@@ -18,12 +18,12 @@ export const nextSentence = () => {
     }
 
     //检查是否处于演出完成状态，不是则结束所有普通演出（保持演出不算做普通演出）
-    let allsettled = true;
+    let allSettled = true;
     runtime_gamePlay.performList.forEach(e => {
         if (!e.isHoldOn)
-            allsettled = false;
+            allSettled = false;
     })
-    if (allsettled) { // 所有普通演出已经结束
+    if (allSettled) { // 所有普通演出已经结束
         //清除状态表的演出序列（因为这时候已经准备进行下一句了）
         if (storeRef.stageRef) {
             const stageStore: any = storeRef.stageRef.current;
@@ -39,7 +39,7 @@ export const nextSentence = () => {
         return;
     }
 
-    //不处于 allsettled 状态，清除所有普通演出，强制进入settled。
+    //不处于 allSettled 状态，清除所有普通演出，强制进入settled。
     logger.info('清除普通演出')
     for (let i = 0; i < runtime_gamePlay.performList.length; i++) {
         const e = runtime_gamePlay.performList[i];
