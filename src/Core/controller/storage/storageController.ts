6a0e31eefb2954d9df8a53ddf4dd56bc25c9cc36
Mahiru <makinoharashoko@outlook.com>
2022-03-26 17:03:27 +0800

    为操作本地存储的函数加上节流

--- src/Core/controller/storage/storageController.ts
+++ src/Core/controller/storage/storageController.ts
@@ -4,14 +4,27 @@ import {getRef} from "../../store/storeRef";
 import {logger} from "../../util/logger";
 import {IUserData} from "../../store/userData";
 
-export const setStorage = () => {
+
+export const setStorage = debounce(() => {
     localforage.setItem(gameInfo.gameKey, getRef('userDataRef').userDataState).then(() => {
         logger.info('写入本地存储');
     });
-}
+}, 100);
 
-export const getStorage = () => {
+export const getStorage = debounce(() => {
     localforage.getItem(gameInfo.gameKey).then(newUserData => {
         getRef('userDataRef').replaceUserData(<IUserData>newUserData);
     })
+}, 100);
+
+function debounce(func: Function, wait: number) {
+    let timeout: any;
+
+    return function (this: any, ...args: any[]) {
+        let context: any = this; // 保存this指向
+        clearTimeout(timeout)
+        timeout = setTimeout(function () {
+            func.apply(context, args)
+        }, wait);
+    }
 }
