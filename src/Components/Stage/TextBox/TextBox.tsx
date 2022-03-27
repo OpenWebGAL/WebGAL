687a434d8d1e668dc3694c47f7bb26a59c1d756e
Mahiru <makinoharashoko@outlook.com>
2022-03-27 17:06:28 +0800

    将事件绑定到一个不常用事件

--- src/Components/Stage/TextBox/TextBox.tsx
+++ src/Components/Stage/TextBox/TextBox.tsx
@@ -2,8 +2,13 @@ import {useStore} from "reto";
 import {stageStateStore} from "../../../Core/store/stage";
 import styles from './textbox.module.scss'
 import {nextSentence} from "../../../Core/controller/gamePlay/nextSentence";
+import {useEffect} from "react";
+import {stopAll} from "../../../Core/controller/gamePlay/fastSkip";
 
 export const TextBox = () => {
+    useEffect(() => {
+
+    })
     const StageStore = useStore(stageStateStore);
     //拆字
     const textArray: Array<string> = StageStore.stageState.showText.split('');
@@ -12,7 +17,10 @@ export const TextBox = () => {
                      key={index + 'textElement' + e}
                      style={{animationDelay: '' + index * 35 + 'ms'}}>{e}</span>;
     })
-    return <div id={'textBoxMain'} className={styles.TextBox_main} onClick={nextSentence}>
+    return <div id={'textBoxMain'} className={styles.TextBox_main} onClick={() => {
+        stopAll();
+        nextSentence();
+    }} onAuxClick={nextSentence}>
         {textElementList}
     </div>
 }
--- src/Core/controller/gamePlay/fastSkip.ts
+++ src/Core/controller/gamePlay/fastSkip.ts
@@ -1,6 +1,28 @@
 //切换自动播放状态
 import {runtime_gamePlay} from "../../runtime/gamePlay";
 
+export const stopFast = () => {
+    runtime_gamePlay.isFast = false;
+    if (runtime_gamePlay.fastInterval !== null) {
+        clearInterval(runtime_gamePlay.fastInterval);
+        runtime_gamePlay.fastInterval = null;
+    }
+}
+
+export const stopAll = () => {
+    runtime_gamePlay.isFast = false;
+    if (runtime_gamePlay.fastInterval !== null) {
+        clearInterval(runtime_gamePlay.fastInterval);
+        runtime_gamePlay.fastInterval = null;
+    }
+
+    runtime_gamePlay.isAuto = false;
+    if (runtime_gamePlay.autoInterval !== null) {
+        clearInterval(runtime_gamePlay.autoInterval);
+        runtime_gamePlay.autoInterval = null;
+    }
+}
+
 export const switchFast = () => {
     //现在正在快进
     if (runtime_gamePlay.isFast) {
@@ -12,7 +34,7 @@ export const switchFast = () => {
     } else { //当前不在快进
         runtime_gamePlay.isFast = true;
         runtime_gamePlay.fastInterval = setInterval(() => {
-            const event = new MouseEvent('click', {
+            const event = new MouseEvent('auxclick', {
                 'view': window,
                 'bubbles': true,
                 'cancelable': true
