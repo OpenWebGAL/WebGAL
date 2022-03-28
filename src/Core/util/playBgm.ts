094537a855db9dc2f7c4fac607d948bc8ee5b142
Mahiru <makinoharashoko@outlook.com>
2022-03-28 20:30:54 +0800

    添加标题界面的起始页与标题bgm

---
+++ src/Core/util/playBgm.ts
@@ -0,0 +1,25 @@
+import {logger} from "./logger";
+import {getRef} from "../store/storeRef";
+
+export const playBgm = (url: string) => {
+    logger.debug('播放bgm');
+    //先停止之前的bgm
+    let VocalControl: any = document.getElementById("currentBgm");
+    if (VocalControl !== null) {
+        VocalControl.currentTime = 0;
+        if (!VocalControl.paused)
+            VocalControl.pause();
+    }
+    //获得舞台状态并设置
+    getRef('stageRef').setStage('bgm', url);
+    //播放语音
+    setTimeout(() => {
+        let VocalControl: any = document.getElementById("currentBgm");
+        if (VocalControl !== null) {
+            VocalControl.currentTime = 0;
+            VocalControl.oncanplay = () => {
+                VocalControl.play();
+            }
+        }
+    }, 1);
+}
