b13b8bbd6f7638a0b0fa835c23d89dbb4229e534
MakinoharaShoko <makinoharashoko@outlook.com>
2022-04-07 10:52:32 +0800

    完成对资源预加载、场景预加载的实现

--- src/Core/util/assetsPrefetcher.ts
+++ src/Core/util/assetsPrefetcher.ts
@@ -1,4 +1,5 @@
 import {IAsset} from "../interface/coreInterface/sceneInterface";
+import {logger} from "./logger";
 
 /**
  * 预加载函数
@@ -6,4 +7,13 @@ import {IAsset} from "../interface/coreInterface/sceneInterface";
  */
 export const assetsPrefetcher = (assetList: Array<IAsset>) => {
     //TODO: 实现预加载函数
+    for (const asset of assetList) {
+        const newLink = document.createElement('link');
+        newLink.setAttribute('rel', 'prefetch');
+        newLink.setAttribute('href', asset.url);
+        const head = document.getElementsByTagName('head');
+        if (head.length) {
+            head[0].appendChild(newLink);
+        }
+    }
 }
