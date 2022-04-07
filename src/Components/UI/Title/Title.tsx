3b88d14bad97b38cb77ff9640d533a22912846f8
MakinoharaShoko <makinoharashoko@outlook.com>
2022-04-07 22:25:58 +0800

    实现原版的Title UI

--- src/Components/UI/Title/Title.tsx
+++ src/Components/UI/Title/Title.tsx
@@ -26,22 +26,26 @@ const Title: FC = () => {
                          }}/>}
                 <div className={styles.Title_buttonList}>
                     <div className={styles.Title_button} onClick={startGame}>
-                        开始游戏
+                        <div className={styles.Title_button_text + ' ' + styles.Title_button_text_up}>开始游戏</div>
+                        <div className={styles.Title_button_text}>START</div>
                     </div>
                     <div className={styles.Title_button} onClick={() => GuiStore.setVisibility('showTitle', false)}>
-                        继续游戏
+                        <div className={styles.Title_button_text + ' ' + styles.Title_button_text_up}>继续游戏</div>
+                        <div className={styles.Title_button_text}>CONTINUE</div>
                     </div>
                     <div className={styles.Title_button} onClick={() => {
                         GuiStore.setVisibility('showMenuPanel', true)
                         GuiStore.setMenuPanelTag(MenuPanelTag.Option)
                     }}>
-                        游戏配置
+                        <div className={styles.Title_button_text + ' ' + styles.Title_button_text_up}>游戏选项</div>
+                        <div className={styles.Title_button_text}>OPTIONS</div>
                     </div>
                     <div className={styles.Title_button} onClick={() => {
                         GuiStore.setVisibility('showMenuPanel', true)
                         GuiStore.setMenuPanelTag(MenuPanelTag.Load)
                     }}>
-                        读取存档
+                        <div className={styles.Title_button_text + ' ' + styles.Title_button_text_up}>读取存档</div>
+                        <div className={styles.Title_button_text}>LOAD</div>
                     </div>
                 </div>
             </div>}
