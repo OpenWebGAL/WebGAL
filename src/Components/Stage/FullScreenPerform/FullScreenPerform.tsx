2eb5e82da1033cc07f378eba63c181797016e1dd
MakinoharaShoko <makinoharashoko@outlook.com>
2022-04-07 20:47:28 +0800

    调整舞台与存读档界面UI

--- src/Components/Stage/FullScreenPerform/FullScreenPerform.tsx
+++ src/Components/Stage/FullScreenPerform/FullScreenPerform.tsx
@@ -1,9 +1,8 @@
 import styles from './fullScreenPerform.module.scss'
-import {nextSentence} from "../../../Core/controller/gamePlay/nextSentence";
 
 export const FullScreenPerform = () => {
     return <div className={styles.FullScreenPerform_main}>
         <div id={'videoContainer'}/>
-        <div onClick={nextSentence} className={styles.introContainer} id={'introContainer'}/>
+        <div className={styles.introContainer} id={'introContainer'}/>
     </div>
 }
