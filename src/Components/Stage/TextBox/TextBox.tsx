ed22b9c155577ecd0ae292f056a8ee0c99f238dc
Mahiru <makinoharashoko@outlook.com>
2022-04-13 16:58:10 +0800

    使设置项生效

--- src/Components/Stage/TextBox/TextBox.tsx
+++ src/Components/Stage/TextBox/TextBox.tsx
@@ -2,23 +2,28 @@ import {useStore} from "reto";
 import {stageStateStore} from "../../../Core/store/stage";
 import styles from './textbox.module.scss';
 import {useEffect} from "react";
+import {userDataStateStore} from "../../../Core/store/userData";
 
 export const TextBox = () => {
     useEffect(() => {
 
     });
     const StageStore = useStore(stageStateStore);
+    const userDataStore = useStore(userDataStateStore);
+    const textDelay = 55 - 20 * userDataStore.userDataState.optionData.textSpeed;
+    const size = userDataStore.userDataState.optionData.textSize * 50 + 200 + '%';
+
     // 拆字
     const textArray: Array<string> = StageStore.stageState.showText.split('');
     const textElementList = textArray.map((e, index) => {
         return <span className={styles.TextBox_textElement_start}
                      key={index + 'textElement' + e + StageStore.stageState.showText}
-                     style={{animationDelay: String(index * 35) + 'ms'}}>{e}</span>;
+                     style={{animationDelay: String(index * textDelay) + 'ms'}}>{e}</span>;
     });
     return <div id="textBoxMain" className={styles.TextBox_main}>
         {StageStore.stageState.showName !== '' &&
-            <div className={styles.TextBox_showName}>{StageStore.stageState.showName}</div>}
-        <div>
+            <div className={styles.TextBox_showName} style={{fontSize: '200%'}}>{StageStore.stageState.showName}</div>}
+        <div style={{fontSize: size}}>
             {textElementList}
         </div>
     </div>;
