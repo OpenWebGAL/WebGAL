250cd6e83f397fb9b9f367f61c156d9815b72034
Mahiru <makinoharashoko@outlook.com>
2022-04-14 09:54:25 +0800

    完成跳转标签

--- src/Core/controller/gamePlay/runScript.ts
+++ src/Core/controller/gamePlay/runScript.ts
@@ -15,6 +15,8 @@ import {miniAvatar} from "../../../Core/controller/gamePlay/scripts/miniAvatar";
 import {pixiInit} from "../../../Core/controller/gamePlay/scripts/pixiInit";
 import {logger} from "../../../Core/util/logger";
 import {playVideo} from "../../../Core/controller/gamePlay/scripts/playVideo";
+import {jumpLabel} from "../../../Core/controller/gamePlay/scripts/jumpLabel";
+import {label} from "../../../Core/controller/gamePlay/scripts/label";
 
 /**
  * 规范函数的类型
@@ -43,7 +45,9 @@ export const runScript = (script: ISentence) => {
         [commandType.pixi,pixi],
         [commandType.miniAvatar,miniAvatar],
         [commandType.pixiInit,pixiInit],
-        [commandType.video,playVideo]
+        [commandType.video,playVideo],
+        [commandType.jumpLabel,jumpLabel],
+        [commandType.label,label],
     ]);
 
     // 根据脚本类型切换函数
