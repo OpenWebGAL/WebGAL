6827485241789da85b7f4ba4148320f73749aabf
Mahiru <makinoharashoko@outlook.com>
2022-04-14 10:49:43 +0800

    完成选项界面的初步设计

--- src/Core/parser/scriptParser/contentParser.ts
+++ src/Core/parser/scriptParser/contentParser.ts
@@ -22,8 +22,35 @@ export const contentParser = (contentRaw: string, type: commandType) => {
         case commandType.miniAvatar:
             return assetSetter(contentRaw, fileType.figure);
         case commandType.video:
-            return assetSetter(contentRaw,fileType.video);
+            return assetSetter(contentRaw, fileType.video);
+        case commandType.choose:
+            return getChooseContent(contentRaw);
         default:
             return contentRaw;
     }
 };
+
+function getChooseContent(contentRaw: string): string {
+    const chooseList = contentRaw.split('|');
+    const chooseKeyList = [];
+    const chooseValueList = [];
+    for (const e of chooseList) {
+        chooseKeyList.push(e.split(':')[0]);
+        chooseValueList.push(e.split(':')[1]);
+    }
+    const parsedChooseList = chooseValueList.map(e => {
+        if (e.match(/\./)) {
+            return assetSetter(e, fileType.scene);
+        } else {
+            return e;
+        }
+    });
+    let ret = '';
+    for (let i = 0; i < chooseKeyList.length; i++) {
+        if (i !== 0) {
+            ret = ret + '|';
+        }
+        ret = ret + `${chooseKeyList[i]}:${parsedChooseList[i]}`;
+    }
+    return ret;
+}
