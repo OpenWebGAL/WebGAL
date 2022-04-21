0a8fc810bd86a9878b4566090d573f292db00e73
Mahiru <Mahiru_@outlook.com>
2022-04-22 00:49:42 +0800

    尝试解决设置页面动画导致界面模糊的问题；解决继续游戏如果游戏未开始时黑屏的问题

--- src/Core/controller/eventBus/eventSender.ts
+++ src/Core/controller/eventBus/eventSender.ts
@@ -1,5 +1,8 @@
 /**
  * 事件创建器，用于将事件发送到指定的元素
+ * targetID = nextSentence_target 继续下一句
+ * targetID = restorePerform_target 还原演出
+ * targetID = setVolume_target 强制同步音量与本地存储
  * @param targetID 元素ID
  * @param message 发送的信息（目前只能为数字）
  * @param timeout 等待时间
