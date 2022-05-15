842e6311ae72f5bab1b5105aef4ba0ed844970ad
Mahiru <Mahiru_@outlook.com>
2022-05-15 11:26:02 +0800

    fix: option text preview animation overlapping

--- src/Components/UI/Menu/Options/TextPreview/TextPreview.tsx
+++ src/Components/UI/Menu/Options/TextPreview/TextPreview.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./textPreview.module.scss";
 import {useSelector} from "react-redux";
 import {RootState} from "@/Core/store/store";
-import {playSpeed} from "@/Core/interface/stateInterface/userDataInterface";
 
 export const TextPreview = (props: any) => {
   const userDataState = useSelector((state: RootState) => state.userData);
@@ -10,22 +9,11 @@ export const TextPreview = (props: any) => {
   const previewText = '现在预览的是文本框字体大小和播放速度的情况，您可以根据您的观感调整上面的选项。';
   const size = userDataState.optionData.textSize * 50 + 200 + '%';
   let classNameText = styles.singleText;
-  switch (userDataState.optionData.textSpeed) {
-  case playSpeed.normal:
-    classNameText = styles.singleText;
-    break;
-  case playSpeed.slow:
-    classNameText = styles.singleText1;
-    break;
-  case playSpeed.fast:
-    classNameText = styles.singleText2;
-    break;
-  }
   const previewTextArray = previewText.split('')
     .map((e, i) => <span id={textDelay + 'text' + i}
       style={{fontSize: size, animationDelay: String(i * textDelay) + 'ms'}}
       className={classNameText}
-      key={e + i}>{e}</span>);
+      key={e + i + userDataState.optionData.textSpeed}>{e}</span>);
   return <div className={styles.textPreviewMain}>
     {previewTextArray}
   </div>;
