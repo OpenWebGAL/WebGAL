92801f93302aa6936f71aaa64e710dd8cfab3ec1
Mahiru <Mahiru_@outlook.com>
2022-05-25 22:47:53 +0800

    feat: complete basic appreciate function.

--- src/Core/controller/stage/playBgm.ts
+++ src/Core/controller/stage/playBgm.ts
@@ -1,29 +1,25 @@
-import {logger} from '../../util/etc/logger';
 import {webgalStore} from "@/Core/store/store";
 import {setStage} from "@/Core/store/stageReducer";
 
+// /**
+//  * 停止bgm
+//  */
+// export const eraseBgm = () => {
+//   logger.debug(`停止bgm`);
+//   // 停止之前的bgm
+//   let VocalControl: any = document.getElementById('currentBgm');
+//   if (VocalControl !== null) {
+//     VocalControl.currentTime = 0;
+//     if (!VocalControl.paused) VocalControl.pause();
+//   }
+//   // 获得舞台状态并设置
+//   webgalStore.dispatch(setStage({key: 'bgm', value: ''}));
+// };
+
 /**
- * 播放bgm的实际执行函数
- * @param url bgm的路径
+ * 播放bgm
+ * @param url bgm路径
  */
-export const playBgm = (url: string) => {
-  logger.debug(`播放bgm：${url}`);
-  // 先停止之前的bgm
-  let VocalControl: any = document.getElementById('currentBgm');
-  if (VocalControl !== null) {
-    VocalControl.currentTime = 0;
-    if (!VocalControl.paused) VocalControl.pause();
-  }
-  // 获得舞台状态并设置
+export function playBgm(url: string): void {
   webgalStore.dispatch(setStage({key: 'bgm', value: url}));
-  // 播放语音
-  setTimeout(() => {
-    let VocalControl: any = document.getElementById('currentBgm');
-    if (VocalControl !== null) {
-      VocalControl.currentTime = 0;
-      VocalControl.oncanplay = () => {
-        VocalControl.play();
-      };
-    }
-  }, 1);
-};
+}
