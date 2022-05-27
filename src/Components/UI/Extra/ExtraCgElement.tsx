45e2e9880ac9d3d09334cc55b57667ec1335d520
Mahiru <Mahiru_@outlook.com>
2022-05-27 13:21:29 +0800

    feat: change choose style, add view full image function of cg

---
+++ src/Components/UI/Extra/ExtraCgElement.tsx
@@ -0,0 +1,40 @@
+import {useObject} from "@/hooks/useObject";
+import styles from "@/Components/UI/Extra/extra.module.scss";
+import React from "react";
+
+interface IProps {
+  name: string;
+  imgUrl: string;
+  transformDeg: number;
+  index: number;
+}
+
+
+export function ExtraCgElement(props: IProps) {
+  const showFull = useObject(false);
+  return <>
+    {showFull.value && <div onClick={() => showFull.set(!showFull.value)} className={styles.showFullContainer}>
+      <div className={styles.showFullCgMain}>
+        <div style={{
+          backgroundImage: `url('${props.imgUrl}')`,
+          backgroundSize: `cover`,
+          backgroundPosition: "center",
+          width: '100%',
+          height: '100%',
+        }}/>
+      </div>
+    </div>}
+    <div onClick={() => showFull.set(!showFull.value)} style={{
+      // transform: `rotate(${deg}deg)`,
+      animation: `cg_softIn_${props.transformDeg} 1.5s ease-out ${100 + props.index * 100}ms forwards `
+    }} key={props.name} className={styles.cgElement}>
+      <div style={{
+        backgroundImage: `url('${props.imgUrl}')`,
+        backgroundSize: `cover`,
+        backgroundPosition: "center",
+        width: '100%',
+        height: '100%',
+      }}/>
+    </div>
+  </>;
+}
