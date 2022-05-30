d50691a4b6357f53fdd8ec4ae58a571f2bd06d0e
Mahiru <makinoharashoko@outlook.com>
2022-05-30 20:08:42 +0800

    fix: prevent repeat preload.

--- src/Core/util/prefetcher/assetsPrefetcher.ts
+++ src/Core/util/prefetcher/assetsPrefetcher.ts
@@ -1,5 +1,6 @@
 import {IAsset} from '../../../interface/coreInterface/sceneInterface';
 import {logger} from '../etc/logger';
+import {settledAssets} from "@/Core/runtime/etc";
 
 /**
  * 预加载函数
@@ -11,15 +12,13 @@ export const assetsPrefetcher = (assetList: Array<IAsset>) => {
     // 是否要插入这个标签
     let isInsert = true;
     // 判断是否已经存在
-    // 查询所有 link 标签，检查是否有相同的 href 属性
-    const links = document.querySelectorAll('link');
-    links.forEach(element => {
-      if (element.getAttribute('href') === asset.url) {
-        return;
+    settledAssets.forEach(settledAssetUrl => {
+      if (settledAssetUrl === asset.url) {
+        isInsert = false;
       }
     });
     if (!isInsert) {
-      logger.warn('已经存在相同的链接，不再插入');
+      logger.warn('该资源已在预加载列表中，无需重复加载');
     } else {
       const newLink = document.createElement('link');
       newLink.setAttribute('rel', 'prefetch');
@@ -28,6 +27,7 @@ export const assetsPrefetcher = (assetList: Array<IAsset>) => {
       if (head.length) {
         head[0].appendChild(newLink);
       }
+      settledAssets.push(asset.url);
     }
   }
 };
