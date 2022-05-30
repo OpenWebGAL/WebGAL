95bc82b0f76abf832ea71092af1b668662908a2a
Mahiru <makinoharashoko@outlook.com>
2022-05-30 20:02:38 +0800

    feat: Improve preload and unregister sw.

--- src/Core/util/prefetcher/assetsPrefetcher.ts
+++ src/Core/util/prefetcher/assetsPrefetcher.ts
@@ -1,18 +1,33 @@
-import { IAsset } from '../../../interface/coreInterface/sceneInterface';
+import {IAsset} from '../../../interface/coreInterface/sceneInterface';
+import {logger} from '../etc/logger';
 
 /**
  * 预加载函数
  * @param assetList 场景资源列表
  */
 export const assetsPrefetcher = (assetList: Array<IAsset>) => {
-  // TODO: 实现预加载函数
+
   for (const asset of assetList) {
-    const newLink = document.createElement('link');
-    newLink.setAttribute('rel', 'prefetch');
-    newLink.setAttribute('href', asset.url);
-    const head = document.getElementsByTagName('head');
-    if (head.length) {
-      head[0].appendChild(newLink);
+    // 是否要插入这个标签
+    let isInsert = true;
+    // 判断是否已经存在
+    // 查询所有 link 标签，检查是否有相同的 href 属性
+    const links = document.querySelectorAll('link');
+    links.forEach(element => {
+      if (element.getAttribute('href') === asset.url) {
+        return;
+      }
+    });
+    if (!isInsert) {
+      logger.warn('已经存在相同的链接，不再插入');
+    } else {
+      const newLink = document.createElement('link');
+      newLink.setAttribute('rel', 'prefetch');
+      newLink.setAttribute('href', asset.url);
+      const head = document.getElementsByTagName('head');
+      if (head.length) {
+        head[0].appendChild(newLink);
+      }
     }
   }
 };
