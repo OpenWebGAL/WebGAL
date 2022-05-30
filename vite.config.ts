adbf18a6ca0ced6925d64a1037319a7727d25a29
Mahiru <makinoharashoko@outlook.com>
2022-05-30 20:24:35 +0800

    chore: add rollup-plugin-visualizer.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import loadVersion from 'vite-plugin-package-version';
-import { resolve } from 'path';
+import {resolve} from 'path';
+import {visualizer} from 'rollup-plugin-visualizer';
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), loadVersion()],
-    resolve: {
-        alias: {
-            '@': resolve('src'),
-        },
+  plugins: [react(), loadVersion(), visualizer()],
+  resolve: {
+    alias: {
+      '@': resolve('src'),
     },
+  },
 });
