e53070c230dc5d89389321368bf997cb7b82f315
Mahiru <makinoharashoko@outlook.com>
2022-05-31 09:41:57 +0800

    feat: add figure transform and filter

---
+++ src/Core/gameScripts/setFigFilter.ts
@@ -0,0 +1,54 @@
+import {ISentence} from '@/interface/coreInterface/sceneInterface';
+import {IPerform} from '@/interface/coreInterface/performInterface';
+import {IEffect} from "@/interface/stateInterface/stageInterface";
+import {logger} from '@/Core/util/etc/logger';
+import {webgalStore} from "@/Core/store/store";
+import {setStage} from "@/Core/store/stageReducer";
+import cloneDeep from 'lodash/cloneDeep';
+
+/**
+ * 设置立绘效果
+ * @param sentence
+ */
+export const setFigFilter = (sentence: ISentence): IPerform => {
+  const stageState = webgalStore.getState().stage;
+  const effectList: Array<IEffect> = stageState.effects;
+  const newEffectList = cloneDeep(effectList);
+  let target = 'figCenterContainer';
+  sentence.args.forEach(e => {
+    if (e.key === 'left' && e.value) {
+      target = 'figLeftContainer';
+    }
+    if (e.key === 'right' && e.value) {
+      target = 'figRightContainer';
+    }
+  });
+  let isTargetSet = false;
+  newEffectList.forEach((e) => {
+    if (e.target === target) {
+      logger.warn('已存在效果，正在修改');
+      isTargetSet = true;
+      e.filter = sentence.content;
+    }
+  });
+  if (!isTargetSet) {
+    newEffectList.push({
+      target: target,
+      transform: '',
+      filter: sentence.content
+    });
+  }
+  webgalStore.dispatch(setStage({key: 'effects', value: newEffectList}));
+  // stageStore.setStage('bgFilter', sentence.content);
+  return {
+    performName: 'none',
+    duration: 0,
+    isOver: false,
+    isHoldOn: false,
+    stopFunction: () => {
+    },
+    blockingNext: () => false,
+    blockingAuto: () => true,
+    stopTimeout: undefined, // 暂时不用，后面会交给自动清除
+  };
+};
