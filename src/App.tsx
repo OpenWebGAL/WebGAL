b6aec11af6eee2c85e9da153be31761f8f197a93
Mahiru <Mahiru_@outlook.com>
2022-06-17 22:08:42 +0800

    feat: initialize film mode.

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,15 @@ import Menu from './Components/UI/Menu/Menu';
 import {MainStage} from './Components/Stage/MainStage';
 import {BottomControlPanel} from './Components/UI/BottomControlPanel/ButtomControlPanel';
 import {Backlog} from './Components/UI/Backlog/Backlog';
-import {Provider} from 'react-redux';
-import {webgalStore} from './Core/store/store';
+import {Provider, useSelector} from 'react-redux';
+import {RootState, webgalStore} from './Core/store/store';
 import {Extra} from "@/Components/UI/Extra/Extra";
+import { BottomControlPanelFilm } from './Components/UI/BottomControlPanel/BottomControlPanelFilm';
 
 function App() {
   useEffect(() => {
     initializeScript();
   }, []);
-
   // Provider用于对各组件提供状态
   return (
     <div className="App" style={{height: '100%', width: '100%', background: 'rgba(0, 0, 0, 1)'}}>
@@ -23,6 +23,7 @@ function App() {
         <Menu/>
         <MainStage/>
         <BottomControlPanel/>
+        <BottomControlPanelFilm/>
         <Backlog/>
       </Provider>
     </div>
