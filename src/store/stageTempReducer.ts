990d07a6d215b21d38654a88d6f16f6bc5a645f6
Mahiru <Mahiru_@outlook.com>
2022-10-03 01:46:41 +0800

    fix: 修复极特殊情况下在读档时出现切换背景效果的问题

--- src/store/stageTempReducer.ts
+++ src/store/stageTempReducer.ts
@@ -2,10 +2,12 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface IStageTempState {
   oldBg: string;
+  oldBgKey: string;
 }
 
 const initState: IStageTempState = {
   oldBg: '',
+  oldBgKey: '',
 };
 
 /**
@@ -15,8 +17,9 @@ const stageTempState = createSlice({
   name: 'stageTemp',
   initialState: initState,
   reducers: {
-    setOldBg: (state, action: PayloadAction<string>) => {
-      state.oldBg = action.payload;
+    setOldBg: (state, action: PayloadAction<{ value: string; key: string }>) => {
+      state.oldBg = action.payload.value;
+      state.oldBgKey = action.payload.key;
     },
   },
 });
