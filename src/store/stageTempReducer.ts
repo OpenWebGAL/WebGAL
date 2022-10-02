c713f76f6439dac89da02d3e7f048e0915dd0e47
Mahiru <Mahiru_@outlook.com>
2022-10-03 00:42:02 +0800

    Update stageTempReducer.ts

--- src/store/stageTempReducer.ts
+++ src/store/stageTempReducer.ts
@@ -1,13 +1,19 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+interface IStageTempState {
+  oldBg: string;
+}
+
+const initState: IStageTempState = {
+  oldBg: '',
+};
+
 /**
  * 舞台临时状态
  */
 const stageTempState = createSlice({
   name: 'stageTemp',
-  initialState: {
-    oldBg: '',
-  },
+  initialState: initState,
   reducers: {
     setOldBg: (state, action: PayloadAction<string>) => {
       state.oldBg = action.payload;
