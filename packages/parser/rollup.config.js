e056b82b708c019e240e88a5d7d54704c28ea875
Mahiru <Mahiru_@outlook.com>
2023-03-10 09:18:15 +0800

    parser: add types and umd

--- packages/parser/rollup.config.js
+++ packages/parser/rollup.config.js
@@ -2,7 +2,7 @@ import typescript from "rollup-plugin-typescript2";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
-const mode = process.env.MODE;
+const mode = process.env.MODE ?? 'prod';
 const isProd = mode === "prod";
 
 export default [
@@ -41,6 +41,27 @@ export default [
           compilerOptions: {
             sourceMap: !isProd,
             declarationDir: "build/cjs"
+          }, include: ["src"]
+        }
+      })],
+  },
+  {
+    input: `./src/index.ts`,
+    output: [
+      {
+        file: "./build/umd/index.global.js",
+        name: 'webgalParser',
+        format: 'iife',
+        sourcemap: !isProd
+      },
+    ],
+    plugins: [
+      resolve(), commonjs(), typescript({
+        useTsconfigDeclarationDir: true,
+        tsconfigOverride: {
+          compilerOptions: {
+            sourceMap: !isProd,
+            declarationDir: "build/types"
           }, include: ["src"]
         }
       })],
