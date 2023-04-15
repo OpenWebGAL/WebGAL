0cae558ff3e9ce64cad7c4404452b785a3c1864e
Mahiru <Mahiru_@outlook.com>
2023-04-15 23:52:59 +0800

    fix: volume problem

--- packages/webgal/src/Core/controller/stage/setVolume.ts
+++ packages/webgal/src/Core/controller/stage/setVolume.ts
@@ -10,12 +10,12 @@ export const setVolume = () => {
   const vocalVol = mainVol * 0.01 * userDataState.optionData.vocalVolume * 0.01;
   const bgmVol = mainVol * 0.01 * userDataState.optionData.bgmVolume * 0.01;
   logger.debug(`设置背景音量：${bgmVol},语音音量：${vocalVol}`);
-  const bgmElement: any = document.getElementById('currentBgm');
-  if (bgmElement) {
-    bgmElement.volume = bgmVol.toString();
-  }
-  const vocalElement: any = document.getElementById('currentVocal');
-  if (bgmElement) {
-    vocalElement.volume = vocalVol.toString();
-  }
+  // const bgmElement: any = document.getElementById('currentBgm');
+  // if (bgmElement) {
+  //   bgmElement.volume = bgmVol.toString();
+  // }
+  // const vocalElement: any = document.getElementById('currentVocal');
+  // if (vocalElement) {
+  //   vocalElement.volume = vocalVol.toString();
+  // }
 };
