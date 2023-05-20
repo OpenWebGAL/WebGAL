ddde6214f63de52f7957b0f11ba5366bf0ab7477
Mahiru <Mahiru_@outlook.com>
2023-05-20 17:08:23 +0800

    feat: language select

--- packages/webgal/src/Components/UI/Translation/Translation.tsx
+++ packages/webgal/src/Components/UI/Translation/Translation.tsx
@@ -1,10 +1,47 @@
 import useLanguage from '@/hooks/useLanguage';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import s from './translation.module.scss';
+import { language } from '@/config/language';
 
 export default function Translation() {
   const setLanguage = useLanguage();
 
-  useEffect(() => setLanguage(Number(window?.localStorage.getItem('lang'))), []);
+  const [isShowSelectLanguage, setIsShowSelectLanguage] = useState(false);
 
-  return null;
+  useEffect(() => {
+    const lang = window?.localStorage.getItem('lang');
+    if (!lang) {
+      setIsShowSelectLanguage(true);
+    } else {
+      setLanguage(Number(window?.localStorage.getItem('lang')));
+    }
+  }, []);
+
+  const setLang = (langId: language) => {
+    setIsShowSelectLanguage(false);
+    setLanguage(langId);
+  };
+
+  return (
+    <>
+      {isShowSelectLanguage && (
+        <div className={s.trans}>
+          <div className={s.langWrapper}>
+            <div className={s.lang}>LANGUAGE SELECT</div>
+            <div className={s.langSelect}>
+              <div className={s.langSelectButton} onClick={() => setLang(language.zhCn)}>
+                中文
+              </div>
+              <div className={s.langSelectButton} onClick={() => setLang(language.en)}>
+                ENGLISH
+              </div>
+              <div className={s.langSelectButton} onClick={() => setLang(language.jp)}>
+                日本語
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
