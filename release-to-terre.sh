84225ca22ba72ea1ce7be1ca36c2be9573f68e39
Mahiru <Mahiru_@outlook.com>
2023-06-18 20:53:42 +0800

    Update release-to-terre.sh

--- release-to-terre.sh
+++ release-to-terre.sh
@@ -12,4 +12,6 @@ rm -r webgal-serviceworker.js
 # 进入 WebGAL 构建目录
 cd ../../../../../../WebGAL/packages/webgal || exit
 # 复制
-cp -r dist/* ../../../WebGAL_Terre/packages/terre2/assets/templates/WebGAL_Template
+cp -r dist/index.html ../../../WebGAL_Terre/packages/terre2/assets/templates/WebGAL_Template
+cp -r dist/assets ../../../WebGAL_Terre/packages/terre2/assets/templates/WebGAL_Template
+cp -r dist/webgal-serviceworker.js ../../../WebGAL_Terre/packages/terre2/assets/templates/WebGAL_Template
