ec12e37d4f54d063e7a4497a3652cff2e8c0c622
Mahiru <Mahiru_@outlook.com>
2023-06-20 18:25:11 +0800

    add long script parse time test

--- packages/parser/test/parser.test.ts
+++ packages/parser/test/parser.test.ts
@@ -49,3 +49,30 @@ test("choose", async () => {
   };
   expect(result.sentenceList).toContainEqual(expectSentenceItem);
 });
+
+test("long-script", async () => {
+
+  const sceneRaw = await fsp.readFile('test/test-resources/long-script.txt');
+  const sceneText = sceneRaw.toString();
+
+  const parser = new SceneParser((assetList) => {
+  }, (fileName, assetType) => {
+    return fileName;
+  }, ADD_NEXT_ARG_LIST, SCRIPT_CONFIG);
+
+  console.log('line count:',sceneText.split('\n').length)
+  console.time('parse-time-consumed')
+  const result = parser.parse(sceneText, "start", "/start.txt");
+  console.timeEnd('parse-time-consumed')
+  const expectSentenceItem: ISentence = {
+    command: commandType.label,
+    commandRaw: "label",
+    content: "end",
+    args: [
+      { key: "next", value: true }
+    ],
+    sentenceAssets: [],
+    subScene: []
+  };
+  expect(result.sentenceList).toContainEqual(expectSentenceItem);
+});
