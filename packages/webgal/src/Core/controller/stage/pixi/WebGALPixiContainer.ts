c6f7ed605b555cacbff2edf87c7142619415a979
YujiSakai <freed.bass@gmail.com>
2023-06-25 23:57:06 +0900

    フィルター機能の追加

--- packages/webgal/src/Core/controller/stage/pixi/WebGALPixiContainer.ts
+++ packages/webgal/src/Core/controller/stage/pixi/WebGALPixiContainer.ts
@@ -1,4 +1,9 @@
 import { OldFilmFilter } from '@pixi/filter-old-film';
+import { DotFilter } from '@pixi/filter-dot';
+import { ReflectionFilter } from '@pixi/filter-reflection';
+import { GlitchFilter } from '@pixi/filter-glitch';
+import { RGBSplitFilter } from '@pixi/filter-rgb-split';
+import { GodrayFilter } from '@pixi/filter-godray';
 import * as PIXI from 'pixi.js';
 
 export class WebGALPixiContainer extends PIXI.Container {
@@ -86,6 +91,131 @@ export class WebGALPixiContainer extends PIXI.Container {
   public set oldFilm(value: number) {
     console.log('set oldfilm');
     this.getOrCreateOldFilmFilter();
+  }
+
+  /**
+   * dot film filter
+   * @private
+   */
+  private getOrCreateDotFilter() {
+    const blurFilterFromMap = this.containerFilters.get('dotFilm');
+    if (blurFilterFromMap) {
+      return blurFilterFromMap;
+    } else {
+      const dotFilm = new DotFilter();
+      this.addFilter(dotFilm);
+      this.containerFilters.set('dotFilm', dotFilm);
+      return dotFilm;
+    }
+  }
+  public get dotFilm(): number {
+    this.getOrCreateDotFilter();
+    return 0;
+  }
+
+  public set dotFilm(value: number) {
+    console.log('set dotFilm');
+    this.getOrCreateDotFilter();
+  }
+
+  /**
+   * reflection film filter
+   * @private
+   */
+  private getOrCreateReflectionFilter() {
+    const blurFilterFromMap = this.containerFilters.get('reflectionFilm');
+    if (blurFilterFromMap) {
+      return blurFilterFromMap;
+    } else {
+      const reflectionFilm = new ReflectionFilter();
+      this.addFilter(reflectionFilm);
+      this.containerFilters.set('reflectionFilm', reflectionFilm);
+      return reflectionFilm;
+    }
+  }
+  public get reflectionFilm(): number {
+    this.getOrCreateReflectionFilter();
+    return 0;
+  }
+
+  public set reflectionFilm(value: number) {
+    console.log('set reflectionFilm');
+    this.getOrCreateReflectionFilter();
+  }
+
+   /**
+   * glitchFilter film filter
+   * @private
+   */
+  private getOrCreateGlitchFilter() {
+    const blurFilterFromMap = this.containerFilters.get('glitchFilm');
+    if (blurFilterFromMap) {
+      return blurFilterFromMap;
+    } else {
+      const glitchFilm = new GlitchFilter();
+      this.addFilter(glitchFilm);
+      this.containerFilters.set('glitchFilm', glitchFilm);
+      return glitchFilm;
+    }
+  }
+  public get glitchFilm(): number {
+    this.getOrCreateGlitchFilter();
+    return 0;
+  }
+
+  public set glitchFilm(value: number) {
+    console.log('set glitchFilm');
+    this.getOrCreateGlitchFilter();
+  }
+
+   /**
+   * rgbSplitFilter film filter
+   * @private
+   */
+  private getOrCreateRGBSplitFilter() {
+    const blurFilterFromMap = this.containerFilters.get('rgbFilm');
+    if (blurFilterFromMap) {
+      return blurFilterFromMap;
+    } else {
+      const rgbFilm = new RGBSplitFilter();
+      this.addFilter(rgbFilm);
+      this.containerFilters.set('rgbFilm', rgbFilm);
+      return rgbFilm;
+    }
+  }
+  public get rgbFilm(): number {
+    this.getOrCreateRGBSplitFilter();
+    return 0;
+  }
+
+  public set rgbFilm(value: number) {
+    console.log('set rgbFilm');
+    this.getOrCreateRGBSplitFilter();
+  }
+
+   /**
+   * godrayFilter film filter
+   * @private
+   */
+  private getOrCreateGodrayFilter() {
+    const blurFilterFromMap = this.containerFilters.get('godrayFilm');
+    if (blurFilterFromMap) {
+      return blurFilterFromMap;
+    } else {
+      const godrayFilm = new GodrayFilter();
+      this.addFilter(godrayFilm);
+      this.containerFilters.set('godrayFilm', godrayFilm);
+      return godrayFilm;
+    }
+  }
+  public get godrayFilm(): number {
+    this.getOrCreateGodrayFilter();
+    return 0;
+  }
+
+  public set godrayFilm(value: number) {
+    console.log('set godrayFilm');
+    this.getOrCreateGodrayFilter();
   }
 
   private addFilter(filter: PIXI.Filter) {
