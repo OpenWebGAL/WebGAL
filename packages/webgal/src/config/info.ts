9787aac9bf5f4349e92dbafdebd97c410ca2beb6
Mahiru <Mahiru_@outlook.com>
2023-07-03 21:49:26 +0800

    update version number

--- packages/webgal/src/config/info.ts
+++ packages/webgal/src/config/info.ts
@@ -1,5 +1,5 @@
 export const __INFO = {
-  version: 'WebGAL 4.4.0-alpha5',
+  version: 'WebGAL 4.4.0',
   contributors: [
     { username: 'Mahiru', link: 'https://github.com/MakinoharaShoko' },
     { username: 'Hoshinokinya', link: 'https://github.com/hshqwq' },
