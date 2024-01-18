4386304cccaffe9dcc87c825d4bca08b8149e436
Mahiru <Mahiru_@outlook.com>
2024-01-18 19:22:13 +0800

    enhance style and script

--- packages/webgal/src/Core/gameScripts/choose/index.tsx
+++ packages/webgal/src/Core/gameScripts/choose/index.tsx
@@ -12,26 +12,13 @@ import { useSEByWebgalStore } from '@/hooks/useSoundEffect';
 import { WebGAL } from '@/Core/WebGAL';
 import { whenChecker } from '@/Core/controller/gamePlay/scriptExecutor';
 
-
 class ChooseOption {
-  text: string;
-  jump: string;
-  jumpToScene: boolean;
-  showCondition?: string;
-  enableCondition?: string;
-
-  constructor(text: string, jump: string) { 
-    this.text = text;
-    this.jump = jump;
-    this.jumpToScene = jump.match(/\./) !== null;
-  }
-
   /**
    * 格式：
-   * (showConditionVar>1)[enableConditionVar>2]=>text:jump
+   * (showConditionVar>1)[enableConditionVar>2]->text:jump
    */
-  static parse(script: string): ChooseOption {
-    const parts = script.split('=>');
+  public static parse(script: string): ChooseOption {
+    const parts = script.split('->');
     const conditonPart = parts.length > 1 ? parts[0] : null;
     const mainPart = parts.length > 1 ? parts[1] : parts[0];
     const mainPartNodes = mainPart.split(':');
@@ -49,12 +36,18 @@ class ChooseOption {
     }
     return option;
   }
+  public text: string;
+  public jump: string;
+  public jumpToScene: boolean;
+  public showCondition?: string;
+  public enableCondition?: string;
 
+  public constructor(text: string, jump: string) {
+    this.text = text;
+    this.jump = jump;
+    this.jumpToScene = jump.match(/\./) !== null;
+  }
 }
-
-
-
-
 
 /**
  * 显示选择枝
@@ -73,15 +66,17 @@ export const choose = (sentence: ISentence): IPerform => {
       .map((e, i) => {
         const enable = whenChecker(e.enableCondition);
         const className = enable ? styles.Choose_item : styles.Choose_item_disabled;
-        const onClick = enable ? () => {
-          playSeClickChoose();
-          if (e.jumpToScene) {
-            changeScene(e.jump, e.text);
-          } else {
-            jmp(e.jump);
-          }
-          WebGAL.gameplay.performController.unmountPerform('choose');
-        } : () => {};
+        const onClick = enable
+          ? () => {
+              playSeClickChoose();
+              if (e.jumpToScene) {
+                changeScene(e.jump, e.text);
+              } else {
+                jmp(e.jump);
+              }
+              WebGAL.gameplay.performController.unmountPerform('choose');
+            }
+          : () => {};
         return (
           <div
             className={className}
@@ -93,8 +88,9 @@ export const choose = (sentence: ISentence): IPerform => {
             {e.text}
           </div>
         );
-      })
+      });
   };
+  // eslint-disable-next-line react/no-deprecated
   ReactDOM.render(
     <div className={styles.Choose_Main}>{runtimeBuildList(chooseOptions)}</div>,
     document.getElementById('chooseContainer'),
@@ -104,6 +100,7 @@ export const choose = (sentence: ISentence): IPerform => {
     duration: 1000 * 60 * 60 * 24,
     isHoldOn: false,
     stopFunction: () => {
+      // eslint-disable-next-line react/no-deprecated
       ReactDOM.render(<div />, document.getElementById('chooseContainer'));
     },
     blockingNext: () => true,
