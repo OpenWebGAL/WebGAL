0ad55d196bd10c4f388ab34501185c6cfc34181d
Mahiru <Mahiru_@outlook.com>
2024-03-27 20:20:41 +0800

    fix: ws port

--- packages/webgal/src/Core/util/syncWithEditor/webSocketFunc.ts
+++ packages/webgal/src/Core/util/syncWithEditor/webSocketFunc.ts
@@ -9,12 +9,22 @@ import { runScript } from '@/Core/controller/gamePlay/runScript';
 export const webSocketFunc = () => {
   const loc: string = window.location.hostname;
   const protocol: string = window.location.protocol;
+  const port: string = window.location.port; // 获取端口号
+
+  // 默认情况下，不需要在URL中明确指定标准HTTP(80)和HTTPS(443)端口
+  let defaultPort = '';
+  if (port && port !== '80' && port !== '443') {
+    // 如果存在非标准端口号，将其包含在URL中
+    defaultPort = `:${port}`;
+  }
+
   if (protocol !== 'http:' && protocol !== 'https:') {
     return;
   }
-  let wsUrl = `ws://${loc}/api/webgalsync`;
+  // 根据当前协议构建WebSocket URL，并包括端口号（如果有）
+  let wsUrl = `ws://${loc}${defaultPort}/api/webgalsync`;
   if (protocol === 'https:') {
-    wsUrl = `wss://${loc}/api/webgalsync`;
+    wsUrl = `wss://${loc}${defaultPort}/api/webgalsync`;
   }
   logger.info('正在启动socket连接位于：' + wsUrl);
   const socket = new WebSocket(wsUrl);
