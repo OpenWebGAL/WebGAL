f38c3e448c32835aba37affa77879d7f1fbbc0d3
Mahiru <Mahiru_@outlook.com>
2024-04-02 23:23:15 +0800

    feat: screen constants, fix: iOS font problem.

--- packages/webgal/src/Core/gameScripts/pixi/performs/rain.ts
+++ packages/webgal/src/Core/gameScripts/pixi/performs/rain.ts
@@ -2,6 +2,7 @@ import * as PIXI from 'pixi.js';
 import { registerPerform } from '@/Core/util/pixiPerformManager/pixiPerformManager';
 
 import { WebGAL } from '@/Core/WebGAL';
+import { SCREEN_CONSTANTS } from '@/Core/util/constants';
 
 const pixiRain = (rainSpeed: number, number: number) => {
   // 动画参数
@@ -27,8 +28,8 @@ const pixiRain = (rainSpeed: number, number: number) => {
   // 监听动画更新
   function ticker(delta: number) {
     // 获取长宽，用于控制雪花出现位置
-    const stageWidth = 2560;
-    const stageHeight = 1440;
+    const stageWidth = SCREEN_CONSTANTS.width;
+    const stageHeight = SCREEN_CONSTANTS.height;
     for (let i = 0; i < number; i++) {
       // 创建对象
       const bunny = new PIXI.Sprite(texture);
