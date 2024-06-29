bf4529dcb9c7b708dad58df987fa1ecd8f019bb7
Mahiru <Mahiru_@outlook.com>
2024-06-29 21:00:30 +0800

    fix: scene fetch error handle

--- packages/webgal/src/Core/controller/scene/callScene.ts
+++ packages/webgal/src/Core/controller/scene/callScene.ts
@@ -24,16 +24,21 @@ export const callScene = (sceneUrl: string, sceneName: string) => {
     continueLine: WebGAL.sceneManager.sceneData.currentSentenceId,
   });
   // 场景写入到运行时
-  sceneFetcher(sceneUrl).then((rawScene) => {
-    WebGAL.sceneManager.sceneData.currentScene = sceneParser(rawScene, sceneName, sceneUrl);
-    WebGAL.sceneManager.sceneData.currentSentenceId = 0;
-    // 开始场景的预加载
-    const subSceneList = WebGAL.sceneManager.sceneData.currentScene.subSceneList;
-    WebGAL.sceneManager.settledScenes.push(sceneUrl); // 放入已加载场景列表，避免递归加载相同场景
-    const subSceneListUniq = uniqWith(subSceneList); // 去重
-    scenePrefetcher(subSceneListUniq);
-    logger.debug('现在调用场景，调用结果：', WebGAL.sceneManager.sceneData);
-    WebGAL.sceneManager.lockSceneWrite = false;
-    nextSentence();
-  });
+  sceneFetcher(sceneUrl)
+    .then((rawScene) => {
+      WebGAL.sceneManager.sceneData.currentScene = sceneParser(rawScene, sceneName, sceneUrl);
+      WebGAL.sceneManager.sceneData.currentSentenceId = 0;
+      // 开始场景的预加载
+      const subSceneList = WebGAL.sceneManager.sceneData.currentScene.subSceneList;
+      WebGAL.sceneManager.settledScenes.push(sceneUrl); // 放入已加载场景列表，避免递归加载相同场景
+      const subSceneListUniq = uniqWith(subSceneList); // 去重
+      scenePrefetcher(subSceneListUniq);
+      logger.debug('现在调用场景，调用结果：', WebGAL.sceneManager.sceneData);
+      WebGAL.sceneManager.lockSceneWrite = false;
+      nextSentence();
+    })
+    .catch((e) => {
+      logger.error('场景调用错误', e);
+      WebGAL.sceneManager.lockSceneWrite = false;
+    });
 };
