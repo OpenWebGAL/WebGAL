dbbd35f42fd7350f1cb078d76bd58ef0047d6c39
xiaoxustudio <1783558957@qq.com>
2024-07-08 00:17:37 +0800

    Fixed bug where globalVariable is empty

--- packages/webgal/src/hooks/useConfigData.ts
+++ packages/webgal/src/hooks/useConfigData.ts
@@ -13,41 +13,42 @@ const useConfigData = (configData: IConfigData) => {
   return useEffect(() => {
     // configData发生变化
     for (let i in configData) {
-      if (_map.includes(i)) {
-        const val = configData[i] as string;
-        switch (i) {
-          case 'Title_img': {
-            const titleUrl = assetSetter(val, fileType.background);
-            webgalStore.dispatch(setGuiAsset({ asset: 'titleBg', value: titleUrl }));
-            setEbg(titleUrl);
-            break;
-          }
+      if (!_map.includes(i)) {
+        continue;
+      }
+      const val = configData[i] as string;
+      switch (i) {
+        case 'Title_img': {
+          const titleUrl = assetSetter(val, fileType.background);
+          webgalStore.dispatch(setGuiAsset({ asset: 'titleBg', value: titleUrl }));
+          setEbg(titleUrl);
+          break;
+        }
 
-          case 'Game_Logo': {
-            const logoUrlList = [assetSetter(val, fileType.background)];
-            webgalStore.dispatch(setLogoImage(logoUrlList));
-            break;
-          }
+        case 'Game_Logo': {
+          const logoUrlList = [assetSetter(val, fileType.background)];
+          webgalStore.dispatch(setLogoImage(logoUrlList));
+          break;
+        }
 
-          case 'Title_bgm': {
-            const bgmUrl = assetSetter(val, fileType.bgm);
-            webgalStore.dispatch(setGuiAsset({ asset: 'titleBgm', value: bgmUrl }));
-            break;
-          }
+        case 'Title_bgm': {
+          const bgmUrl = assetSetter(val, fileType.bgm);
+          webgalStore.dispatch(setGuiAsset({ asset: 'titleBgm', value: bgmUrl }));
+          break;
+        }
 
-          case 'Game_name': {
-            WebGAL.gameName = val;
-            document.title = val;
-            break;
-          }
+        case 'Game_name': {
+          WebGAL.gameName = val;
+          document.title = val;
+          break;
+        }
 
-          case 'Game_key': {
-            WebGAL.gameKey = val;
-            getStorage();
-            getFastSaveFromStorage();
-            getSavesFromStorage(0, 0);
-            break;
-          }
+        case 'Game_key': {
+          WebGAL.gameKey = val;
+          getStorage();
+          getFastSaveFromStorage();
+          getSavesFromStorage(0, 0);
+          break;
         }
       }
     }
