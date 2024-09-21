41cca41e6bd8ec16947dc94eb715e8caa1c8945a
Mahiru <Mahiru_@outlook.com>
2024-09-21 15:38:50 +0800

    feat: add break line process

---
+++ packages/parser/test/debug-linebreak.ts
@@ -0,0 +1,12 @@
+import {sceneTextPreProcess} from "../src/sceneTextPreProcessor";
+import *  as fsp from "fs/promises";
+
+
+async function debug() {
+  const sceneRaw = await fsp.readFile('test/test-resources/line-break.txt');
+  const sceneText = sceneRaw.toString();
+  const result = sceneTextPreProcess(sceneText)
+  console.log(result)
+}
+
+debug();
