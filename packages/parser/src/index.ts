5cc1c7b2e553beca3329ea990f9c6118a0ccca39
Mahiru <Mahiru_@outlook.com>
2024-11-12 21:57:16 +0800

    fix: temporary restore old parser

--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -9,8 +9,15 @@ import { subSceneScanner } from './scriptParser/subSceneScanner';
 import { uniqWith } from 'lodash';
 import { IWebGALStyleObj, scss2cssinjsParser } from './styleParser';
 export { SyntaxError as parserSyntaxError } from './parser';
+import {
+  ADD_NEXT_ARG_LIST,
+  SCRIPT_CONFIG,
+  ConfigMap,
+  ConfigItem,
+} from './config/scriptConfig';
+import { sceneParser } from './sceneParser';
 
-export default class SceneParser {
+export class NewSceneParser {
 
     private readonly assetsPrefetcher;
     private readonly assetSetter;
@@ -87,3 +94,70 @@ export default class SceneParser {
     }
 
 }
+
+export default class SceneParser {
+  private readonly SCRIPT_CONFIG_MAP: ConfigMap;
+  constructor(
+    private readonly assetsPrefetcher: (assetList: IAsset[]) => void,
+    private readonly assetSetter: (
+      fileName: string,
+      assetType: fileType,
+    ) => string,
+    private readonly ADD_NEXT_ARG_LIST: number[],
+    SCRIPT_CONFIG_INPUT: ConfigItem[] | ConfigMap,
+  ) {
+    if (Array.isArray(SCRIPT_CONFIG_INPUT)) {
+      this.SCRIPT_CONFIG_MAP = new Map();
+      SCRIPT_CONFIG_INPUT.forEach((config) => {
+        this.SCRIPT_CONFIG_MAP.set(config.scriptString, config);
+      });
+    } else {
+      this.SCRIPT_CONFIG_MAP = SCRIPT_CONFIG_INPUT;
+    }
+  }
+  /**
+   * 解析场景
+   * @param rawScene 原始场景
+   * @param sceneName 场景名称
+   * @param sceneUrl 场景url
+   * @return 解析后的场景
+   */
+  parse(rawScene: string, sceneName: string, sceneUrl: string) {
+    return sceneParser(
+      rawScene,
+      sceneName,
+      sceneUrl,
+      this.assetsPrefetcher,
+      this.assetSetter,
+      this.ADD_NEXT_ARG_LIST,
+      this.SCRIPT_CONFIG_MAP,
+    );
+  }
+
+  parseConfig(configText: string) {
+    return configParser(configText);
+  }
+
+  stringifyConfig(config: WebgalConfig) {
+    return config.reduce(
+      (previousValue, curr) =>
+        previousValue +
+        `${curr.command}:${curr.args.join('|')}${
+          curr.options.length <= 0
+            ? ''
+            : curr.options.reduce(
+              (p, c) => p + ' -' + c.key + '=' + c.value,
+              '',
+            )
+        };\n`,
+      '',
+    );
+  }
+
+  parseScssToWebgalStyleObj(scssString: string): IWebGALStyleObj{
+    return scss2cssinjsParser(scssString);
+  }
+
+}
+
+export { ADD_NEXT_ARG_LIST, SCRIPT_CONFIG };
