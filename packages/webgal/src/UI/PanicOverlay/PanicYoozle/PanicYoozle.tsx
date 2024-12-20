3534bdd9da270f6d9e22e6dada889bed593939dd
Mahiru <Mahiru_@outlook.com>
2024-12-21 01:31:05 +0800

    Update PanicYoozle.tsx

--- packages/webgal/src/UI/PanicOverlay/PanicYoozle/PanicYoozle.tsx
+++ packages/webgal/src/UI/PanicOverlay/PanicYoozle/PanicYoozle.tsx
@@ -17,17 +17,17 @@ export const PanicYoozle = () => {
           <span className={styles.yoozle_blue} style={{ marginRight: '1px' }}>
             W
           </span>
-          <span className={styles.yoozle_red}>e</span>
+          <span className={`${styles.yoozle_red}`}>e</span>
           <span className={styles.yoozle_yellow}>b</span>
           <span className={styles.yoozle_blue}>g</span>
-          <span className={styles.yoozle_green}>a</span>
-          <span className={`${styles.yoozle_red} ${styles.yoozle_e_rotate}`}>l</span>
+          <span className={`${styles.yoozle_green} ${styles.yoozle_e_rotate}`}>a</span>
+          <span className={`${styles.yoozle_red}`}>l</span>
         </span>
       </div>
       <div className={styles.yoozle_search}>
         <input className={styles.yoozle_search_bar} type="text" defaultValue="" />
         <div className={styles.yoozle_search_buttons}>
-          <input className={styles.yoozle_button} type="submit" value="Yoozle Search" />
+          <input className={styles.yoozle_button} type="submit" value="WebGAL Search" />
           <input className={styles.yoozle_button} type="submit" value="Feeling Lucky" />
         </div>
       </div>
