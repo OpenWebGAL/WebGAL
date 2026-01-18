6456087cf379b3aa7e8bec983907fa1758a4fa73
xiaoxustudio <1783558957@qq.com>
2026-01-18 15:34:05 +0800

    fix: sentence save end-of-line comment

--- packages/parser/src/scriptParser/scriptParser.ts
+++ packages/parser/src/scriptParser/scriptParser.ts
@@ -102,6 +102,12 @@ export const scriptParser = (
       args.push(e);
     }
   }
+
+  // 保留注释
+  if (sentenceComment.trim() !== '') {
+    args.push({ key: 'comment', value: sentenceComment.trim() });
+  }
+
   content = contentParser(newSentenceRaw.trim(), command, assetSetter); // 将语句内容里的文件名转为相对或绝对路径
   sentenceAssets = assetsScanner(command, content, args); // 扫描语句携带资源
   subScene = subSceneScanner(command, content); // 扫描语句携带子场景
