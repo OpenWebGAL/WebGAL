759bfaa41205e7e79bd14940859061d416b101fd
Akirami <66513481+A-kirami@users.noreply.github.com>
2026-08-15 00:51:06 +0800

    chore: 移除构建产物中的 gz 压缩文件

--- packages/webgal/vite.config.ts
+++ packages/webgal/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import loadVersion from 'vite-plugin-package-version';
 import { resolve } from 'path';
 import Info from 'unplugin-info/vite';
-import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 
@@ -16,9 +15,6 @@ export default defineConfig({
     react(),
     loadVersion(),
     Info(),
-    viteCompression({
-      filter: /^(.*assets).*\.(js|css|ttf)$/,
-    }),
     // @ts-ignore
     // visualizer(),
   ],
